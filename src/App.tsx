fdf1947577706b2e30dacf5d60693abc255d6e1f
Jorge Poveda <jpoveda731@gmail.com>
2024-01-23 17:32:11 -0500

    feat: creating protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import * as React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { UserContextProvider } from './contexts/UserContext'
-
 import './App.css'
-
-import Layout from './components/layout'
-import Login from './pages/login'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import EmployeeDashboard from './pages/employee/dashboard'
+import { AuthProvider } from './contexts/UserContext'
+import Layout from './components/layout/Layout'
+import Login from './pages/login'
+import { ProtectedRouteAuth, ProtectedRouteRol } from './utils/ProtectedRoute'
 
 export default function App() {
   return (
-    <UserContextProvider>
+    <AuthProvider>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
           <Route element={<Layout />}>
-            <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
+            <Route element={<ProtectedRouteAuth redirectTo="/login" />} >
+              <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
+              <Route element={<ProtectedRouteRol redirectTo="/login" canRolActived="client" />} >
+                <Route path="/employee/dashboard" element={<EmployeeDashboard />} />
+
+              </Route>
+
+            </Route>
           </Route>
         </Routes>
       </BrowserRouter>
-    </UserContextProvider>
+    </AuthProvider >
   )
 }
--- src/pages/employee/dashboard/index.tsx
+++ src/pages/employee/dashboard/index.tsx
@@ -1,6 +1,11 @@
+
+import { useAuth } from '../../../contexts/UserContext'
 import { TaskList, ClassList, CalendarWidget, MessagesWidget } from './widgets'
 
 export default function EmployeeDashboard() {
+  const { user } = useAuth()
+
+  console.log('employee user->', user)
   return (
     <>
       <div className="w-full sm:w-1/2">
