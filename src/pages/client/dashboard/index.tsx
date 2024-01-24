970b5813ec13a225b0a1c48a3bbf1a67676ce99f
dolsen0 <derek.olsen@gmail.com>
2024-01-24 10:55:18 -0500

    added client facilities list

--- src/pages/client/dashboard/index.tsx
+++ src/pages/client/dashboard/index.tsx
@@ -1,7 +1,10 @@
+import { useAuth } from "../../../contexts/AuthContext"
 import { TaskList, ClassList, CalendarWidget, MessagesWidget } from './widgets'
 
 export default function ClientDashboard() {
+  const { user } = useAuth()
 
+  console.log('employee user->', user)
   return (
     <>
       <div className="w-full sm:w-1/2">
