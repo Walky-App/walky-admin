913320f1f0aefa44e152988e31bdc862e379a910
Alex Artemov <artemov.sandro@gmail.com>
2024-01-25 16:28:34 -0500

    fixed Button2 colors switching from red to green

--- src/components/shared/GlobalTable.tsx
+++ src/components/shared/GlobalTable.tsx
@@ -137,8 +137,8 @@ function Button2({ content, onClick, active, disabled }: any) {
   return (
     <button
       className={`flex flex-col cursor-pointer items-center justify-center w-9 h-9 shadow-[0_4px_10px_rgba(0,0,0,0.03)] text-sm font-normal transition-colors rounded-lg
-      ${active ? 'bg-red-500 text-white' : 'text-red-500'}
-      ${!disabled ? 'bg-white hover:bg-red-500 hover:text-white' : 'text-red-300 bg-white cursor-not-allowed'}
+      ${active ? 'bg-green-500 text-green-500' : 'text-green-500'}
+      ${!disabled ? 'bg-white hover:bg-green-500 hover:text-white' : 'text-green-300 bg-white cursor-not-allowed'}
       `}
       onClick={onClick}
       disabled={disabled}>
