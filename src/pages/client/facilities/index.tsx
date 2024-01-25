eb809e7764c469b8dc5919741852463b4302da28
dolsen0 <derek.olsen@gmail.com>
2024-01-25 13:11:20 -0500

    added global table component into client facilities list. Updated navigation path in global table as it previously added an additional slash when navigating to a new page

--- src/pages/client/facilities/index.tsx
+++ src/pages/client/facilities/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 
 import TitleComponent from '../../../components/shared/general/TitleComponent'
 import FacilitiesListItem from './components/FacilitiesListItem'
+import GlobalTable from '../../../components/shared/GlobalTable'
 import { RequestService } from '../../../services/RequestService'
 import { Link } from 'react-router-dom'
 
@@ -17,17 +18,33 @@ export default function Facilities() {
     getFacilities()
   }, []) 
 
+  const facilitiesColumns = [
+    { Header: 'Name', accessor: 'name' },
+    { Header: 'Address', accessor: 'address' },
+    { Header: 'Phone Number', accessor: 'phone_number' },
+    { Header: 'City', accessor: 'city' },
+    { Header: 'State', accessor: 'state' },
+    { Header: 'Zip', accessor: 'zip' },
+    { Header: 'Country', accessor: 'country' },
+  ];
+
   return (
     <div className="mx-auto max-w-screen-xl px-4 py-10 sm:px-6 lg:px-8">
       <TitleComponent title={'Facilities'} />
 
-      <ul className="grid mt-10 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
+
+      <div className="flex flex-col gap-4">
+      <GlobalTable data={facilities} columns={facilitiesColumns} />
+    </div>
+
+
+      {/* <ul className="grid mt-10 grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-2">
         {facilities?.map((facility: any) => (
           <Link key={facility._id} to={`/client/facilities/${facility._id}`}>
             <FacilitiesListItem facility={facility} />
           </Link>
         ))}
-      </ul>
+      </ul> */}
     </div>
   )
 }
