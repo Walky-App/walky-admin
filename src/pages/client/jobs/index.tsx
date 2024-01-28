327f542a019e0ec6f9b8be778e88a2404a7d69a7
dolsen0 <derek.olsen@gmail.com>
2024-01-28 01:23:33 -0500

    added border radius to facility image

--- src/pages/client/jobs/index.tsx
+++ src/pages/client/jobs/index.tsx
@@ -30,7 +30,7 @@ export default function Facilities() {
   ]
 
   return (
-    <div className="mx-auto max-w-screen-xl px-4 py-10 sm:px-6 lg:px-8">
+    <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
       <TitleComponent title={'Jobs by Facility'} />
 
   
