c5adef056db1418010ac2def8ccb90c5ab10afea
Alex Artemov <artemov.sandro@gmail.com>
2024-01-28 18:28:44 -0500

    fixed nav button in admin/users page

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -33,7 +33,7 @@ export default function AdminUsers() {
       <button
         type="button"
         onClick={() => {
-          window.location.href = '/admin/users/add'
+          window.location.href = '/admin/users/new'
         }}
         className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
         >
