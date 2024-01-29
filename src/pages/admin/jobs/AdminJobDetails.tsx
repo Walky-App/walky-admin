037f58ff0bc8a52bdefc4ff51f107c41582f497c
Alex Artemov <artemov.sandro@gmail.com>
2024-01-29 01:33:26 -0500

    got rid of comments in AdminJobDetails

--- src/pages/admin/jobs/AdminJobDetails.tsx
+++ src/pages/admin/jobs/AdminJobDetails.tsx
@@ -1,31 +1,3 @@
-// import { useState, useEffect } from 'react'
-// import { useParams } from 'react-router-dom'
-// import { RequestService } from '../../../services/RequestService'
-
-// export default function AdminJobDetails() {
-//   const [jobData, setJobState] = useState<any>({})
-//   const { id } = useParams()
-
-//   const fetchJob = async () => {
-//     const jobData = await RequestService(`jobs/${id}`)
-//     setJobState(jobData)
-//     console.log(jobData)
-//   }
-
-//   useEffect(() => {
-//     fetchJob()
-//   }, [])
-
-//   return (
-//     <h2>
-//       Job detail view {id} 
-//       {jobData?.title}
-//     </h2>
-    
-//   )
-// }
-
-
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { RequestService } from '../../../services/RequestService';
