4f6ea60115898ef9e0fcc62cd1369dd08d34c1ef
dolsen0 <derek.olsen@gmail.com>
2024-01-29 12:19:05 -0500

    added dropdown for states

--- src/pages/client/jobs/AddJob.tsx
+++ src/pages/client/jobs/AddJob.tsx
@@ -356,17 +356,24 @@ export default function NewJob() {
               </div>
 
               <div className="sm:col-span-2">
-                <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
+                <label htmlFor="state" className="block text-sm font-medium leading-6 text-gray-900">
                   State / Province
                 </label>
                 <div className="mt-2">
-                  <input
-                    type="text"
+                  <select
                     name="state"
                     id="state"
-                    autoComplete="address-level1"
                     className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  />
+                    defaultValue="">
+                    <option value="" disabled>
+                      Select your state
+                    </option>
+                    {states.map(state => (
+                      <option key={state.code} value={state.code}>
+                        {state.name}
+                      </option>
+                    ))}
+                  </select>
                 </div>
               </div>
 
