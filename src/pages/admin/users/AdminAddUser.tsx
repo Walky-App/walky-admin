335cd2913ba88dd3763a7b2272d1e569487c67ba
Alex Artemov <artemov.sandro@gmail.com>
2024-01-28 22:50:42 -0500

    greyed out unnecessary fields

--- src/pages/admin/users/AdminAddUser.tsx
+++ src/pages/admin/users/AdminAddUser.tsx
@@ -113,6 +113,7 @@ export default function AdminAddUser() {
                     name="phone_number"
                     id="phone_number"
                     autoComplete="phone_number"
+                    disabled
                     className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
                   />
                 </div>
