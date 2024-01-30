fdaa0e657fc9b1ba70dd423d5f5c828f9f16c47b
dolsen0 <derek.olsen@gmail.com>
2024-01-30 13:04:32 -0500

    added create job button to jobs list. removed add job button from sidebar

--- src/pages/client/jobs/index.tsx
+++ src/pages/client/jobs/index.tsx
@@ -50,8 +50,16 @@ export default function Facilities() {
   ]
 
   return (
-    <div className="flex flex-col gap-4">
+    <div className="mx-auto max-w-screen-xl px-4  sm:px-6 lg:px-8">
       <TitleComponent title={'Jobs'} />
+      <button
+        type="button"
+        onClick={() => {
+          window.location.href = '/client/jobs/new'
+        }}
+        className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
+        Add Job
+      </button>
       <GlobalTable data={jobsData} columns={jobsColumns} />
     </div>
   )
