367f9d919502b18ee3327f950b0745761bb8d523
dolsen0 <derek.olsen@gmail.com>
2024-01-30 12:55:00 -0500

    removed add facility from sidebar. Add new facility button to facilities list page

--- src/pages/client/facilities/index.tsx
+++ src/pages/client/facilities/index.tsx
@@ -32,6 +32,14 @@ export default function Facilities() {
   return (
     <div className="mx-auto max-w-screen-xl px-4  sm:px-6 lg:px-8">
       <TitleComponent title={'Facilities'} />
+      <button
+        type="button"
+        onClick={() => {
+          window.location.href = '/client/facilities/new'
+        }}
+        className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
+        Add Facility
+      </button>
 
       <div className="flex flex-col gap-4">
         {facilities.length > 0 ? (
