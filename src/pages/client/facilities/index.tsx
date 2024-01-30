5369d2667e52457f7ac18bd639583b361ec40ef1
dolsen0 <derek.olsen@gmail.com>
2024-01-30 15:42:15 -0500

    pulled from main to avoid merge conflicts

--- src/pages/client/facilities/index.tsx
+++ src/pages/client/facilities/index.tsx
@@ -37,7 +37,6 @@ export default function Facilities() {
       <button
         type="button"
         onClick={() => {
-          // window.location.href = '/client/facilities/new'
           navigate('/client/facilities/new')
         }}
         className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
