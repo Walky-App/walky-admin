f9fd568332f60aa23675118b2bd4e694264900b9
Alex Artemov <artemov.sandro@gmail.com>
2024-01-30 23:25:01 -0500

    added status column with active and disabled features to admin facilityDetails

--- src/pages/admin/facilities/index.tsx
+++ src/pages/admin/facilities/index.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import GlobalTable from '../../../components/shared/GlobalTable'
 import { RequestService } from '../../../services/RequestService'
 import HeaderComponent from '../../../components/shared/general/HeaderComponent'
+import { useNavigate } from 'react-router-dom'
 
 export default function AdminFacilities() {
   const [facilitiesData, setFacilitiesData] = React.useState<any>([])
+  const navigate = useNavigate()
 
   React.useEffect(() => {
     const getFacilities = async () => {
@@ -14,29 +16,37 @@ export default function AdminFacilities() {
 
     getFacilities()
   }, [])
-
-  const facilitiesColumns = [
+  
+  const memoFacilitiesData = React.useMemo(() => facilitiesData, [facilitiesData])
+  
+  const memoFacilitiesColumns = React.useMemo(() => [
     { Header: 'Name', accessor: 'name' },
     { Header: 'Address', accessor: 'address' },
     { Header: 'Phone Number', accessor: 'phone_number' },
+    { Header: 'Status', 
+      accessor: 'active', 
+      Cell: ({ value }: { value: boolean }) => value ? <span> Active</span> : <span>Disabled</span>
+    },
+      
     { Header: 'City', accessor: 'city' },
     { Header: 'State', accessor: 'state' },
     { Header: 'Zip', accessor: 'zip' },
     { Header: 'Country', accessor: 'country' },
-  ]
+  ], [])
+
 
   return (
-    <div className="px-20">
+    <div className="">
       <HeaderComponent title={'Facilities'} />
       <button
         type="button"
         onClick={() => {
-          window.location.href = '/admin/facilities/new'
+          navigate ('/admin/facilities/new')
         }}
         className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
         Add New Facility
       </button>
-      <GlobalTable data={facilitiesData} columns={facilitiesColumns} />
+      <GlobalTable data={memoFacilitiesData} columns={memoFacilitiesColumns} />
     </div>
   )
 }
