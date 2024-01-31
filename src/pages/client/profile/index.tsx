fe5dc37c73491e343fa3dd8e3973f6f5e7195335
dolsen0 <derek.olsen@gmail.com>
2024-01-31 17:02:01 -0500

    updated client profile form to handle checkboxes

--- src/pages/client/profile/index.tsx
+++
@@ -1,8 +0,0 @@
-import ProfileForm from './ClientProfile'
-import { GetTokenInfo } from '../../../utils/TokenUtils'
-
-export default function ClientProfile() {
-  const { _id } = GetTokenInfo()
-
-  return <ProfileForm userId={_id} />
-}
