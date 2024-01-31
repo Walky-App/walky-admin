2ff28543eb1e537b1ce6162c731b3ed48bcd5a33
Alex Artemov <artemov.sandro@gmail.com>
2024-01-31 00:53:17 -0500

    added spinner for admin users global table page, all spinners work for all admin pages

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -1,21 +1,33 @@
+import React from 'react'
 import { useState, useEffect } from 'react'
 import GlobalTable from '../../../components/shared/GlobalTable'
 import { RequestService } from '../../../services/RequestService'
 import HeaderComponent from '../../../components/shared/general/HeaderComponent'
+import { useNavigate } from 'react-router-dom'
 
 export default function AdminUsers() {
   const [usersData, setUsersData] = useState<any>([])
+  const [isLoading, setIsLoading] = React.useState(true)
+  const navigate = useNavigate()
 
-  useEffect(() => {
+  React.useEffect(() => {
     const getUsers = async () => {
+      try {
       const allUsers = await RequestService('users')
       setUsersData(allUsers)
+    } catch (error) {
+      console.error('Error fetching user data:', error)
+    } finally {
+      setIsLoading(false)
     }
+  }
 
     getUsers()
   }, [])
 
-  const usersColumns = [
+  const memoUsersData = React.useMemo(() => usersData, [usersData])
+
+  const memoUsersColumns = React.useMemo(() => [
     { Header: 'First Name', accessor: 'first_name' },
     { Header: 'Last Name', accessor: 'last_name' },
     { Header: 'Role', accessor: 'role' },
@@ -25,7 +37,7 @@ export default function AdminUsers() {
     { Header: 'State', accessor: 'state' },
     { Header: 'Zip', accessor: 'zip' },
     { Header: 'Country', accessor: 'country' },
-  ]
+  ], []);
 
   return (
     <div>
@@ -33,12 +45,18 @@ export default function AdminUsers() {
       <button
         type="button"
         onClick={() => {
-          window.location.href = '/admin/users/new'
+          navigate ('/admin/users/new')
         }}
         className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
         Add New User
       </button>
-      <GlobalTable data={usersData} columns={usersColumns} />
-    </div>
+      {isLoading ? (
+      <div className="flex justify-center items-center">
+        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-green-600"></div>
+      </div>
+    ) : (
+      <GlobalTable data={memoUsersData} columns={memoUsersColumns} />
+    )}
+  </div>
   )
 }
