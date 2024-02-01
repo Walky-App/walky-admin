0a867883ad9dd4284e73ad02d1d88206d87dbfd3
jonathans199 <jonathans199@gmail.com>
2024-02-01 13:48:35 -0500

    updated avatar uploading feature

--- src/pages/admin/users/index.tsx
+++ src/pages/admin/users/index.tsx
@@ -13,31 +13,34 @@ export default function AdminUsers() {
   React.useEffect(() => {
     const getUsers = async () => {
       try {
-      const allUsers = await RequestService('users')
-      setUsersData(allUsers)
-    } catch (error) {
-      console.error('Error fetching user data:', error)
-    } finally {
-      setIsLoading(false)
+        const allUsers = await RequestService('users')
+        setUsersData(allUsers)
+      } catch (error) {
+        console.error('Error fetching user data:', error)
+      } finally {
+        setIsLoading(false)
+      }
     }
-  }
 
     getUsers()
   }, [])
 
   const memoUsersData = React.useMemo(() => usersData, [usersData])
 
-  const memoUsersColumns = React.useMemo(() => [
-    { Header: 'First Name', accessor: 'first_name' },
-    { Header: 'Last Name', accessor: 'last_name' },
-    { Header: 'Role', accessor: 'role' },
-    { Header: 'Email', accessor: 'email' },
-    { Header: 'Phone Number', accessor: 'phone_number' },
-    { Header: 'City', accessor: 'city' },
-    { Header: 'State', accessor: 'state' },
-    { Header: 'Zip', accessor: 'zip' },
-    { Header: 'Country', accessor: 'country' },
-  ], []);
+  const memoUsersColumns = React.useMemo(
+    () => [
+      { Header: 'First Name', accessor: 'first_name' },
+      { Header: 'Last Name', accessor: 'last_name' },
+      { Header: 'Role', accessor: 'role' },
+      { Header: 'Email', accessor: 'email' },
+      { Header: 'Phone Number', accessor: 'phone_number' },
+      { Header: 'City', accessor: 'city' },
+      { Header: 'State', accessor: 'state' },
+      { Header: 'Zip', accessor: 'zip' },
+      { Header: 'Country', accessor: 'country' },
+    ],
+    [],
+  )
 
   return (
     <div>
@@ -45,18 +48,18 @@ export default function AdminUsers() {
       <button
         type="button"
         onClick={() => {
-          navigate ('/admin/users/new')
+          navigate('/admin/users/new')
         }}
         className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
         Add New User
       </button>
       {isLoading ? (
-      <div className="flex justify-center items-center">
-        <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-green-600"></div>
-      </div>
-    ) : (
-      <GlobalTable data={memoUsersData} columns={memoUsersColumns} />
-    )}
-  </div>
+        <div className="flex justify-center items-center">
+          <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-green-600"></div>
+        </div>
+      ) : (
+        <GlobalTable data={memoUsersData} columns={memoUsersColumns} />
+      )}
+    </div>
   )
 }
