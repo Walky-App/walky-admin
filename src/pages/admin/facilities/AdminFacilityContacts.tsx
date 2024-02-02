3ed9c97f18952132eb7e68ad0fd90ce6d24c147f
Alex Artemov <artemov.sandro@gmail.com>
2024-02-01 19:51:16 -0500

    added facility contacts page with table and add contact form

---
+++ src/pages/admin/facilities/AdminFacilityContacts.tsx
@@ -0,0 +1,259 @@
+
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { RequestService } from '../../../services/RequestService'
+import { CheckCircleIcon } from '@heroicons/react/20/solid'
+
+export default function AdminFacilityContacts() {
+
+    const { facilityId } = useParams()
+    const { contactId } = useParams()
+    const [facilityContacts, setFacilityContacts] = useState<any>([])
+    const [formData, setFormData] = useState<any>([{}])
+    const [updateSuccess, setUpdateSuccess] = useState<boolean>(false)
+
+
+    useEffect(() => {
+        const getFacilityContacts = async () => {
+            try {
+                const facilityFound = await RequestService(`facilities/${facilityId}`)
+                const facilityContacts = facilityFound.contacts
+                setFacilityContacts(facilityContacts)
+            } catch (error) {
+                console.error('Error fetching facility contacts', error)
+            }
+        }
+        getFacilityContacts()
+    }, [facilityContacts])
+
+    const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+
+        const target = e.target as typeof e.target & {
+            first_name: { value: string }
+            last_name: { value: string }
+            role: { value: string }
+            phone_number: { value: string }
+            email: { value: string }
+        }
+
+        const formData = {
+            first_name: target.first_name.value,
+            last_name: target.last_name.value,
+            role: target.role.value,
+            phone_number: target.phone_number.value,
+            email: target.email.value,
+        }
+
+
+        //updating a facility contact selected by clicking edit button
+
+        // fetch(`${process.env.REACT_APP_PUBLIC_API}/facilities/${facilityId}/contacts/${contactId}`, {
+        //     method: 'PATCH',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         // Authorization: `Bearer ${user?.access_token}`,
+        //     },
+        //     body: JSON.stringify(contactData),
+        // })
+        // }
+
+        //adding the new contact to the facility contacts
+        fetch(`${process.env.REACT_APP_PUBLIC_API}/facilities/${facilityId}/contacts`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                // Authorization: `Bearer ${user?.access_token}`,
+            },
+            body: JSON.stringify(formData),
+        })
+    }
+
+    return (
+        <div className="px-4 sm:px-6 lg:px-8">
+            <div className="sm:flex sm:items-center">
+                <div className="sm:flex-auto">
+                    <h1 className="text-base font-semibold leading-6 text-gray-900">List of contacts</h1>
+                    <p className="mt-2 text-sm text-gray-700">
+                        A list of all the contacts of the selected facility including owner, AP, Onsite.
+                    </p>
+                </div>
+            </div>
+            
+            <div className="mt-8 flow-root">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    {facilityContacts && facilityContacts.length > 0 ? (
+                        <table className="min-w-full divide-y divide-gray-300">
+                            <thead>
+                                <tr>
+                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                        First Name
+                                    </th>
+                                    <th scope="col" className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-0">
+                                        Last Name
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Role
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Email
+                                    </th>
+                                    <th scope="col" className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">
+                                        Phone number
+                                    </th>
+                                    <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                                        <span className="sr-only">Edit</span>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody className="divide-y divide-gray-200">
+                                {facilityContacts.map((contact: any) => (
+                                    <tr key={contact.email}>
+                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            {contact.first_name}
+                                        </td>
+                                        <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
+                                            {contact.last_name}
+                                        </td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{contact.role}</td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{contact.email}</td>
+                                        <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{contact.phone_number}</td>
+                                        <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                                            <a href="#" className="text-indigo-600 hover:text-indigo-900">
+                                                Edit<span className="sr-only">, {contact.first_name} {contact.last_name}</span>
+                                            </a>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    ) : (
+                        // No contacts found
+                        <div className="flex items-center justify-center h-96">
+                            <div className="text-center">
+                                <h2 className="text-3xl font-semibold text-gray-900">No contacts found</h2>
+                                <p className="mt-1 text-sm text-gray-500">Add a contact to the facility</p>
+                            </div>
+                        </div>
+                    )}
+                        {/* Add contact section */}
+
+                        <div className="border-b border-gray-200 pb-5 w-full mb-12 pt-10" >
+                            <h3 className="text-base font-semibold leading-6 text-gray-900">Add contact: </h3>
+                        </div>
+
+                        <form onSubmit={handleForm}>
+                            <div className="space-y-12">
+                                <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+                                    <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
+                                        <div className="sm:col-span-3">
+                                            <label htmlFor="first_name" className="block text-sm font-medium leading-6 text-gray-900">
+                                                First name
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="first_name"
+                                                    id="first_name"
+                                                    autoComplete="given-name"
+                                                    className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-3">
+                                            <label htmlFor="last_name" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Last name
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="last_name"
+                                                    id="last_name"
+                                                    autoComplete="family-name"
+                                                    className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Email address
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="email"
+                                                    name="email"
+                                                    type="email"
+                                                    autoComplete="email"
+                                                    className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-2">
+                                            <label htmlFor="phone_number" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Phone number
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="phone_number"
+                                                    id="phone_number"
+                                                    autoComplete="phone_number"
+                                                    className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+
+                                        <div className="sm:col-span-2">
+                                            <label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">
+                                                Role
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    type="text"
+                                                    name="role"
+                                                    id="role"
+                                                    autoComplete="role"
+                                                    className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div className="mt-6 flex items-center justify-end gap-x-6">
+                                {updateSuccess && (
+                                    <div className="rounded-md bg-green-50 p-4">
+                                        <div className="flex">
+                                            <div className="flex-shrink-0">
+                                                <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
+                                            </div>
+                                            <div className="ml-3">
+                                                <p className="text-sm font-medium text-green-800">Contact successfully updated</p>
+                                            </div>
+                                            <div className="ml-auto pl-3">
+                                                <div className="-mx-1.5 -my-1.5"></div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                                <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
+                                    Cancel
+                                </button>
+                                <button
+                                    type="submit"
+                                    className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
+                                    Submit
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
