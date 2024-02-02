72312bf1f32b0f728e33217ef89999dcd5bfb2b2
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-02-02 16:12:34 -0500

    adding AdminFacilityHeader component

--- src/pages/admin/facilities/AdminFacilityDetails.tsx
+++ src/pages/admin/facilities/AdminFacilityDetails.tsx
@@ -2,20 +2,12 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { RequestService } from '../../../services/RequestService'
 import {
-  CheckCircleIcon,
-  BriefcaseIcon,
-  ChevronDownIcon,
-  MapPinIcon,
-  DocumentPlusIcon,
-  IdentificationIcon,
-  MapIcon,
-  PaperClipIcon,
-  PhotoIcon,
-  UserCircleIcon,
+  CheckCircleIcon, ChevronDownIcon, PhotoIcon,
 } from '@heroicons/react/20/solid'
 import { Fragment } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { classNames } from '../../../utils/Tailwind'
+import AdminFacilityHeaderInfo from './AdminFacilityHeader'
 
 export default function AdminFacilityDetails() {
   const { facilityId } = useParams()
@@ -61,111 +53,48 @@ export default function AdminFacilityDetails() {
   }
   return (
     <>
-      <div className="lg:flex lg:items-center lg:justify-between border-b border-gray-300 mb-20 w-full ">
-        <div className="min-w-0 flex-1">
-          <div className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
-            {formFacility?.name}
-            <div className="flex items-center gap-x-2">
-              <MapPinIcon className="h-10 w-5 text-gray-400" aria-hidden="true" />
-              <h2 className="text-sm font-light text-gray-500">{formFacility?.city}</h2>
-              <MapIcon className="h-10 w-5 text-gray-400" aria-hidden="true" />
-              <h2 className="text-sm font-light text-gray-500">{formFacility?.address}</h2>
+      <AdminFacilityHeaderInfo formFacility={formFacility} />
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 
-              <span className="text-xl text-gray-700"></span>
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 flex lg:ml-4 lg:mt-0 ">
-          <span className="hidden sm:block">
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              <UserCircleIcon className="-ml-0.5 mr-1.5 h-5 w-5 " aria-hidden="true" />
-              Contacts
-            </button>
-          </span>
+        {/* Dropdown */}
+        <Menu as="div" className="relative ml-3 sm:hidden">
+          <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
+            More
+            <ChevronDownIcon className="-mr-1 ml-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
+          </Menu.Button>
 
-          <span className="ml-3 hidden sm:block">
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              <PhotoIcon className="-ml-0.5 mr-1.5 h-5 w-5 " aria-hidden="true" />
-              Images
-            </button>
-          </span>
-
-          <span className="ml-3 hidden sm:block">
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              <BriefcaseIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-              Jobs
-            </button>
-          </span>
-          <span className="ml-3 hidden sm:block">
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              <PaperClipIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-              Notes
-            </button>
-          </span>
-          <span className="ml-3 hidden sm:block">
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              <IdentificationIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-              Licenses
-            </button>
-          </span>
-          <span className="ml-3 hidden sm:block">
-            <button
-              type="button"
-              className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              <DocumentPlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" aria-hidden="true" />
-              Docs
-            </button>
-          </span>
-
-          {/* Dropdown */}
-          <Menu as="div" className="relative ml-3 sm:hidden">
-            <Menu.Button className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:ring-gray-400">
-              More
-              <ChevronDownIcon className="-mr-1 ml-1.5 h-5 w-5 text-gray-400" aria-hidden="true" />
-            </Menu.Button>
-
-            <Transition
-              as={Fragment}
-              enter="transition ease-out duration-200"
-              enterFrom="transform opacity-0 scale-95"
-              enterTo="transform opacity-100 scale-100"
-              leave="transition ease-in duration-75"
-              leaveFrom="transform opacity-100 scale-100"
-              leaveTo="transform opacity-0 scale-95">
-              <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                      Edit
-                    </a>
-                  )}
-                </Menu.Item>
-                <Menu.Item>
-                  {({ active }) => (
-                    <a
-                      href="#"
-                      className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
-                      View
-                    </a>
-                  )}
-                </Menu.Item>
-              </Menu.Items>
-            </Transition>
-          </Menu>
-        </div>
+          <Transition
+            as={Fragment}
+            enter="transition ease-out duration-200"
+            enterFrom="transform opacity-0 scale-95"
+            enterTo="transform opacity-100 scale-100"
+            leave="transition ease-in duration-75"
+            leaveFrom="transform opacity-100 scale-100"
+            leaveTo="transform opacity-0 scale-95">
+            <Menu.Items className="absolute right-0 z-10 -mr-1 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                    Edit
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                    View
+                  </a>
+                )}
+              </Menu.Item>
+            </Menu.Items>
+          </Transition>
+        </Menu>
       </div>
+
 
       <form onSubmit={handleUpdate}>
         <div className="space-y-12">
---
+++ src/pages/admin/facilities/AdminFacilityHeader.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import {
+  MapPinIcon,
+  MapIcon,
+  UserCircleIcon,
+  PhotoIcon,
+  BriefcaseIcon,
+  PaperClipIcon,
+  IdentificationIcon,
+  DocumentPlusIcon,
+} from '@heroicons/react/20/solid'
+import { JSX } from 'react/jsx-runtime';
+
+const AdminFacilityHeaderInfo = ({ formFacility }: { formFacility: any }) => {
+  return (
+    <div className="lg:flex lg:items-center lg:justify-between border-b border-gray-300 mb-20 w-full">
+      <div className="min-w-0 flex-1">
+        <div className="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
+          {formFacility?.name}
+          <div className="flex items-center gap-x-2">
+            <MapPinIcon className="h-10 w-5 text-gray-400" aria-hidden="true" />
+            <h2 className="text-sm font-light text-gray-500">{formFacility?.city}</h2>
+            <MapIcon className="h-10 w-5 text-gray-400" aria-hidden="true" />
+            <h2 className="text-sm font-light text-gray-500">{formFacility?.address}</h2>
+            <span className="text-xl text-gray-700"></span>
+          </div>
+        </div>
+      </div>
+      <div className="mt-5 flex lg:ml-4 lg:mt-0">
+        {renderButton('Contacts', <UserCircleIcon className="-ml-0.5 mr-1.5 h-5 w-5 " />)}
+        {renderButton('Images', <PhotoIcon className="-ml-0.5 mr-1.5 h-5 w-5 " />)}
+        {renderButton('Jobs', <BriefcaseIcon className="-ml-0.5 mr-1.5 h-5 w-5" />)}
+        {renderButton('Notes', <PaperClipIcon className="-ml-0.5 mr-1.5 h-5 w-5" />)}
+        {renderButton('Licenses', <IdentificationIcon className="-ml-0.5 mr-1.5 h-5 w-5" />)}
+        {renderButton('Docs', <DocumentPlusIcon className="-ml-0.5 mr-1.5 h-5 w-5" />)}
+      </div>
+    </div>
+  );
+};
+
+const renderButton = (label: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | null | undefined, icon: string | number | boolean | Iterable<React.ReactNode> | React.PromiseLikeOfReactNode | JSX.Element | null | undefined) => (
+  <span className="ml-3 hidden sm:block">
+    <button
+      type="button"
+      className="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+    >
+      {icon}
+      {label}
+    </button>
+  </span>
+);
+
+export default AdminFacilityHeaderInfo;
