f00330411df66609ec8f61475a1fc4af330463df
jonathans199 <jonathans199@gmail.com>
2024-02-02 13:45:18 -0500

    updated conditional avatar for all profiles

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -97,11 +97,15 @@ export default function Header() {
               <Menu.Button className="relative flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2">
                 <span className="absolute -inset-1.5" />
                 <span className="sr-only">Open user menu</span>
-                <img
-                  className="h-8 w-8 rounded-full"
-                  src={user ? user.avatar : 'https://ui-avatars.com/api/?name=John+Doe'}
-                  alt=""
-                />
+                {user && user.avatar ? (
+                  <img className="h-8 w-8 rounded-full" src={user.avatar} alt="" />
+                ) : (
+                  <span className="inline-block h-8 w-8 overflow-hidden rounded-full bg-gray-100">
+                    <svg className="h-full w-full text-gray-300" fill="currentColor" viewBox="0 0 24 24">
+                      <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+                    </svg>
+                  </span>
+                )}
               </Menu.Button>
             </div>
             <Transition
