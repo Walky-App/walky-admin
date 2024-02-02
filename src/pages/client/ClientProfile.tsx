4a21b079f7c5c25f096a0f92a8433fcab6e06949
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-02-02 14:07:51 -0500

    cancel button removed of every admin form

--- src/pages/client/ClientProfile.tsx
+++ src/pages/client/ClientProfile.tsx
@@ -504,9 +504,6 @@ export default function ClientProfile() {
                 </div>
               </div>
             )}
-            <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-              Cancel
-            </button>
             <button
               type="submit"
               className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
