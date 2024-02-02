5ddd87dc0038e86de1ab71640daea69ad027fabd
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-02-02 11:54:36 -0500

    cleaning the AdminFacilityDetails file

--- src/pages/admin/facilities/AdminFacilityDetails.tsx
+++ src/pages/admin/facilities/AdminFacilityDetails.tsx
@@ -1,26 +1,8 @@
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { RequestService } from '../../../services/RequestService'
-import {
-  CheckCircleIcon,
-  DocumentPlusIcon,
-  IdentificationIcon,
-  MapIcon,
-  PaperClipIcon,
-  PhotoIcon,
-  UserCircleIcon,
-} from '@heroicons/react/20/solid'
+import {CheckCircleIcon,BriefcaseIcon,ChevronDownIcon,MapPinIcon,DocumentPlusIcon,IdentificationIcon,MapIcon,PaperClipIcon,PhotoIcon,UserCircleIcon,} from '@heroicons/react/20/solid'
 import { Fragment } from 'react'
-import {
-  BriefcaseIcon,
-  CalendarIcon,
-  CheckIcon,
-  ChevronDownIcon,
-  CurrencyDollarIcon,
-  LinkIcon,
-  MapPinIcon,
-  PencilIcon,
-} from '@heroicons/react/20/solid'
 import { Menu, Transition } from '@headlessui/react'
 
 function classNames(...classes: any) {
