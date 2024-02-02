89c55aae7eb385e6360f11de06373e10a6dd6a46
Alex Artemov <artemov.sandro@gmail.com>
2024-02-02 17:07:40 -0500

    admin facility contacts page: add contact and update contact working properly

--- src/pages/admin/facilities/AdminFacilityContacts.tsx
+++ src/pages/admin/facilities/AdminFacilityContacts.tsx
@@ -1,15 +1,20 @@
-import React, { useEffect, useState } from 'react'
+import React, { use, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { RequestService } from '../../../services/RequestService'
 import { CheckCircleIcon } from '@heroicons/react/20/solid'
+import AdminFacilityContactModal from './AdminFacilityContactModal'
 
 export default function AdminFacilityContacts() {
   const { facilityId } = useParams()
   const { contactId } = useParams()
+  const [currentContact, setCurrentContact] = useState<any>({})
   const [facilityContacts, setFacilityContacts] = useState<any>([])
   const [facilityName, setFacilityName] = useState<string>('')
   const [formData, setFormData] = useState<any>([{}])
   const [updateSuccess, setUpdateSuccess] = useState<boolean>(false)
+
+  //Modal state
+  const [openModal, setOpenModal] = useState(false)
 
   useEffect(() => {
     const getFacilityContacts = async () => {
@@ -24,9 +29,9 @@ export default function AdminFacilityContacts() {
       }
     }
     getFacilityContacts()
-  }, [facilityContacts])
+  }, [])
 
-  const handleForm = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleAddContactForm = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     const target = e.target as typeof e.target & {
@@ -45,20 +50,7 @@ export default function AdminFacilityContacts() {
       email: target.email.value,
     }
 
-    //updating a facility contact selected by clicking edit button
-
-    // fetch(`${process.env.REACT_APP_PUBLIC_API}/facilities/${facilityId}/contacts/${contactId}`, {
-    //     method: 'PATCH',
-    //     headers: {
-    //         'Content-Type': 'application/json',
-    //         // Authorization: `Bearer ${user?.access_token}`,
-    //     },
-    //     body: JSON.stringify(contactData),
-    // })
-    // }
-
-    //adding the new contact to the facility contacts
-    fetch(`${process.env.REACT_APP_PUBLIC_API}/facilities/${facilityId}/contacts`, {
+    const res = await fetch(`${process.env.REACT_APP_PUBLIC_API}/facilities/${facilityId}/contacts`, {
       method: 'PATCH',
       headers: {
         'Content-Type': 'application/json',
@@ -66,6 +58,19 @@ export default function AdminFacilityContacts() {
       },
       body: JSON.stringify(formData),
     })
+
+    const result = await res.json()
+
+    if (result) {
+      setFacilityContacts(result.contacts)
+      setFormData(result)
+    }
+  }
+
+  const handleEditButtonClick = (contact: any) => {
+    console.log('contact inside edit button click->', contact)
+    setCurrentContact(contact)
+    setOpenModal(true)
   }
 
   return (
@@ -77,7 +82,7 @@ export default function AdminFacilityContacts() {
         <h3 className="text-base font-semibold leading-6 text-gray-900">Add contact: </h3>
       </div>
 
-      <form onSubmit={handleForm}>
+      <form onSubmit={handleAddContactForm}>
         <div className="space-y-12">
           <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
             <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
@@ -140,19 +145,22 @@ export default function AdminFacilityContacts() {
                   />
                 </div>
               </div>
-
               <div className="sm:col-span-2">
                 <label htmlFor="role" className="block text-sm font-medium leading-6 text-gray-900">
                   Role
                 </label>
                 <div className="mt-2">
-                  <input
-                    type="text"
+                  <select
                     name="role"
                     id="role"
                     autoComplete="role"
-                    className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
-                  />
+                    className="block w-full rounded-md border-0 py-1.5 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6">
+                    <option value="">Select a role</option> {/* Optional: prompt the user to make a selection */}
+                    <option value="Owner">Owner</option>
+                    <option value="AP">AP</option>
+                    <option value="Onsite">Onsite</option>
+                    <option value="Security">Security</option>
+                  </select>
                 </div>
               </div>
             </div>
@@ -175,9 +183,6 @@ export default function AdminFacilityContacts() {
               </div>
             </div>
           )}
-          <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-            Cancel
-          </button>
           <button
             type="submit"
             className="rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
@@ -185,6 +190,8 @@ export default function AdminFacilityContacts() {
           </button>
         </div>
       </form>
+
+      {/* Contacts Table */}
 
       <div className="sm:flex sm:items-center">
         <div className="sm:flex-auto">
@@ -223,28 +230,36 @@ export default function AdminFacilityContacts() {
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200">
-                  {facilityContacts.map((contact: any) => (
-                    <tr key={contact.email}>
+                  {facilityContacts.map((singleContact: any) => (
+                    <tr key={singleContact._id}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                        {contact.first_name}
+                        {singleContact.first_name}
                       </td>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">
-                        {contact.last_name}
+                        {singleContact.last_name}
                       </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{contact.role}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{contact.email}</td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{contact.phone_number}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{singleContact.role}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{singleContact.email}</td>
+                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                        {singleContact.phone_number}
+                      </td>
                       <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                        <a href="#" className="text-green-600 hover:text-green-900">
+                        <button
+                          onClick={() => handleEditButtonClick(singleContact)}
+                          className="text-green-600 hover:text-green-900">
                           Edit
-                          <span className="sr-only">
-                            , {contact.first_name} {contact.last_name}
-                          </span>
-                        </a>
+                        </button>
                       </td>
                     </tr>
                   ))}
                 </tbody>
+                <AdminFacilityContactModal
+                  openModal={openModal}
+                  setOpenModal={setOpenModal}
+                  facilityId={facilityId}
+                  currentContact={currentContact}
+                  setFacilityContacts={setFacilityContacts}
+                />
               </table>
             ) : (
               // No contacts found
@@ -255,7 +270,6 @@ export default function AdminFacilityContacts() {
                 </div>
               </div>
             )}
-            {/* Add contact section */}
           </div>
         </div>
       </div>
