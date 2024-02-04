aec17aa717ed74925b2e72b75096378851e0ba77
Alex Artemov <artemov.sandro@gmail.com>
2024-02-03 23:51:59 -0500

    removed spacing

--- src/pages/admin/facilities/AdminFacilityDetails.tsx
+++ src/pages/admin/facilities/AdminFacilityDetails.tsx
@@ -59,8 +59,6 @@ export default function AdminFacilityDetails() {
     }
 
     RequestService(`/facilities/${facilityId}`, 'PATCH', formValues)
-
-  
   }
 
   if (!facility) return <div>Loading...</div>
