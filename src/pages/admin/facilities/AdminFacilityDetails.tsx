12b623f0df67d8b962ec7ce0c6cde36b2942d304
Alex Artemov <artemov.sandro@gmail.com>
2024-02-06 05:49:05 -0500

    removed business documents upload fields from facilities details

--- src/pages/admin/facilities/AdminFacilityDetails.tsx
+++ src/pages/admin/facilities/AdminFacilityDetails.tsx
@@ -350,67 +350,6 @@ export default function AdminFacilityDetails() {
               </div>
             </div>
           </div>
-
-          <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
-            <div>
-              <h2 className="text-base font-semibold leading-7 text-gray-900">Business License Document</h2>
-              <p className="mt-1 text-sm leading-6 text-gray-600">
-                Please upload business license documents. Please make sure your upload is clear without any warped or
-                blur portions and shows all relevant information.{' '}
-              </p>
-            </div>
-
-            {/* section 3 */}
-
-            <div className="max-w-2xl space-y-10 md:col-span-2">
-              <fieldset>
-                <legend className="text-sm font-semibold leading-6 text-gray-900">
-                  Upload State License Document*
-                </legend>
-                <div className="col-span-full">
-                  <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                    State License
-                  </label>
-                  <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                    <div className="text-center">
-                      <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                      <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer rounded-md bg-white font-semibold text-green-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-600 focus-within:ring-offset-2 hover:text-green-500">
-                          <span>Upload a file</span>
-                          <input id="state_license" name="state_license" type="file" className="sr-only" />
-                        </label>
-                        <p className="pl-1">or drag and drop</p>
-                      </div>
-                      <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                    </div>
-                  </div>
-                </div>
-                <legend className="text-sm font-semibold leading-6 text-gray-900">Upload City License Document*</legend>
-                <div className="col-span-full">
-                  <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                    City License
-                  </label>
-                  <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                    <div className="text-center">
-                      <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                      <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                        <label
-                          htmlFor="file-upload"
-                          className="relative cursor-pointer rounded-md bg-white font-semibold text-green-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-600 focus-within:ring-offset-2 hover:text-green-500">
-                          <span>Upload a file</span>
-                          <input id="city_license" name="city_license" type="file" className="sr-only" />
-                        </label>
-                        <p className="pl-1">or drag and drop</p>
-                      </div>
-                      <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                    </div>
-                  </div>
-                </div>
-              </fieldset>
-            </div>
-          </div>
         </div>
 
         <div className="mt-6 flex items-center justify-end gap-x-6">
