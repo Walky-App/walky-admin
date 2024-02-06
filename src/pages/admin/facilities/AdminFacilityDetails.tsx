a88bd9f92dee0fc2ca2067c1bb558e3ffdbcf6f1
Alex Artemov <artemov.sandro@gmail.com>
2024-02-06 06:36:22 -0500

    removed facility image upload field from facility details page

--- src/pages/admin/facilities/AdminFacilityDetails.tsx
+++ src/pages/admin/facilities/AdminFacilityDetails.tsx
@@ -183,7 +183,7 @@ export default function AdminFacilityDetails() {
 
               <div className="sm:col-span-3">
                 <label htmlFor="phone-number" className="block text-sm font-medium leading-6 text-gray-900">
-                  Business Contact Mobile Number*
+                  Facility Phone Number
                 </label>
                 <div className="mt-2">
                   <input
@@ -230,27 +230,6 @@ export default function AdminFacilityDetails() {
                   />
                 </div>
                 <p className="mt-3 text-sm leading-6 text-gray-600">Write notes about the facility.</p>
-              </div>
-
-              <div className="col-span-full">
-                <label htmlFor="facility-photo" className="block text-sm font-medium leading-6 text-gray-900">
-                  Facility photo
-                </label>
-                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
-                  <div className="text-center">
-                    <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
-                    <div className="mt-4 flex text-sm leading-6 text-gray-600">
-                      <label
-                        htmlFor="file-upload"
-                        className="relative cursor-pointer rounded-md bg-white font-semibold text-green-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-green-600 focus-within:ring-offset-2 hover:text-green-500">
-                        <span>Upload a file</span>
-                        <input id="file-upload" name="main_image" type="file" className="sr-only" />
-                      </label>
-                      <p className="pl-1">or drag and drop</p>
-                    </div>
-                    <p className="text-xs leading-5 text-gray-600">PNG, JPG, GIF up to 10MB</p>
-                  </div>
-                </div>
               </div>
             </div>
           </div>
