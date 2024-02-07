78109f72cf6754bedb3fae8332c48fc5c0d02836
Alex Artemov <artemov.sandro@gmail.com>
2024-02-06 20:56:08 -0500

    moved the checkboxes down below the arrival notes

--- src/pages/admin/facilities/AdminAddFacility.tsx
+++ src/pages/admin/facilities/AdminAddFacility.tsx
@@ -181,6 +181,22 @@ export default function AdminAddFacility() {
                 </div>
               </div>
 
+              <div className="col-span-full">
+                <label htmlFor="notes" className="block text-sm font-medium leading-6 text-gray-900">
+                  Arrival Notes
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="notes"
+                    name="notes"
+                    rows={3}
+                    className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
+                    defaultValue={''}
+                  />
+                </div>
+                <p className="mt-3 text-sm leading-6 text-gray-600">Write notes about the facility.</p>
+              </div>
+
               <fieldset>
                 <legend className="text-sm font-semibold leading-6 text-gray-900">Services</legend>
                 <div className="mt-6 space-y-6">
@@ -298,24 +314,6 @@ export default function AdminAddFacility() {
                   </div>
                 </div>
               </fieldset>
-
-              {/* Section 2 */}
-
-              <div className="col-span-full">
-                <label htmlFor="notes" className="block text-sm font-medium leading-6 text-gray-900">
-                  Notes
-                </label>
-                <div className="mt-2">
-                  <textarea
-                    id="notes"
-                    name="notes"
-                    rows={3}
-                    className="px-3 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
-                    defaultValue={''}
-                  />
-                </div>
-                <p className="mt-3 text-sm leading-6 text-gray-600">Write notes about the facility.</p>
-              </div>
             </div>
           </div>
 
