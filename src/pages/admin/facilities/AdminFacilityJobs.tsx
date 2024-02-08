4087c987bb3265c15be7d6e52ca797aad826893e
Alex Artemov <artemov.sandro@gmail.com>
2024-02-08 17:36:03 -0500

    Displaying 'no jobs found' message if no jobs are returned from API

--- src/pages/admin/facilities/AdminFacilityJobs.tsx
+++ src/pages/admin/facilities/AdminFacilityJobs.tsx
@@ -1,69 +1,69 @@
-import React, {useState, useEffect, useMemo} from "react"
-import { useParams, useNavigate } from "react-router-dom"
-import GlobalTable from "../../../components/shared/GlobalTable"
-import { RequestService } from "../../../services/RequestService"
-import HeaderComponent from "../../../components/shared/general/HeaderComponent"
-import AdminFacilityHeaderInfo from "./AdminFacilityHeader"
+import React, { useState, useEffect, useMemo } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import GlobalTable from '../../../components/shared/GlobalTable';
+import { RequestService } from '../../../services/RequestService';
 
 export default function AdminFacilityJobs() {
-    const { facilityId } = useParams()
-    const [facilityJobs, setFacilityJobs] = React.useState<any>([])
-    const [isLoading, setIsLoading] = React.useState(true)
-    const navigate = useNavigate()
-  
-    React.useEffect(() => {
-      const getFacilityJobs = async () => {
-        try {
-          const facilityJobs = await RequestService(`jobs/facility/${facilityId}`)
-          console.log('jobs by facility ---> ', facilityJobs)
-          setFacilityJobs(facilityJobs)
-        } catch (error) {
-          console.error('Error fetching facility jobs data:', error)
-        } finally {
-          setIsLoading(false)
+  const { facilityId } = useParams();
+  const [facilityJobs, setFacilityJobs] = useState<any>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const getFacilityJobs = async () => {
+      try {
+        const facilityJobs = await RequestService(`jobs/facility/${facilityId}`);
+        if (facilityJobs && Array.isArray(facilityJobs)) {
+          setFacilityJobs(facilityJobs);
+        } else {
+          console.log('No facility jobs found or unexpected data structure received, setting default value.');
+          setFacilityJobs([]);
         }
+      } catch (error) {
+        console.error('Error fetching facility jobs data:', error);
+      } finally {
+        setIsLoading(false);
       }
-  
-      getFacilityJobs()
-    }, [])
-    
+    };
 
-    const memoFacilityJobs = useMemo(() => facilityJobs, [facilityJobs])
+    getFacilityJobs();
+  }, [facilityId]); // Ensure useEffect is run whenever facilityId changes
 
-    const memoFacilityJobsColumns = React.useMemo(
-        () => [
-          { Header: 'Job Title', accessor: 'title' },
-          //@ts-ignore
-          { Header: 'Status', accessor: 'status' },
-          { Header: 'Salary', accessor: 'salary' },
-          //@ts-ignore
-          { Header: 'Skills', accessor: 'skills', Cell: ({ value }) => value.join(', ') },
-          { Header: 'Employment Type', accessor: 'employment_type' },
-        ],
-        [],
-      )
-  
-    return (
-        <>
-        {/* <AdminFacilityHeaderInfo facilityId={facilityId} /> */}
+  const memoFacilityJobsColumns = useMemo(
+    () => [
+      { Header: 'Job Title', accessor: 'title' },
+      { Header: 'Status', accessor: 'status' },
+      { Header: 'Salary', accessor: 'salary' },
+      //@ts-ignore
+      { Header: 'Skills', accessor: 'skills', Cell: ({ value }) => value.join(', ') },
+      { Header: 'Employment Type', accessor: 'employment_type' },
+    ],
+    [],
+  );
+
+  return (
+    <>
       <div>
         <button
           type="button"
-          onClick={() => {
-            navigate(`/admin/facilities/${facilityId}/jobs/new`)
-          }}
+          onClick={() => navigate(`/admin/facilities/${facilityId}/jobs/new`)}
           className="mb-4 rounded-md bg-green-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-green-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-600">
-          Add New Job
+          Add Job
         </button>
-  
+
         {isLoading ? (
           <div className="flex justify-center items-center">
             <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-green-600"></div>
           </div>
+        ) : facilityJobs.length > 0 ? (
+          <GlobalTable data={facilityJobs} columns={memoFacilityJobsColumns} />
         ) : (
-          <GlobalTable data={memoFacilityJobs} columns={memoFacilityJobsColumns} />
+            <div className="text-left">
+            <h2 className="text-3xl font-semibold text-gray-900">No jobs found for this facility</h2>
+            <p className="mt-1 text-sm text-gray-500">Please add a new job</p>
+          </div>
         )}
       </div>
-      </>
-    )
-  }
+    </>
+  );
+}
