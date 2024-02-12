297790cdddc683be5270db8fc813cc901ee4da33
Alex Artemov <artemov.sandro@gmail.com>
2024-02-12 11:59:30 -0500

    setTimeout for AdminFacilityContacts && refresh input fields upon successful submission

--- src/pages/admin/facilities/AdminFacilityContacts.tsx
+++ src/pages/admin/facilities/AdminFacilityContacts.tsx
@@ -54,6 +54,7 @@ export default function AdminFacilityContacts() {
     if (res) {
       setFacilityContacts(res)
       setUpdateSuccess(true)
+      setTimeout(() => setUpdateSuccess(false), 5000) // Hide message after 5 seconds
       form.reset()
     } else {
       console.error('Error adding contact')
@@ -177,7 +178,7 @@ export default function AdminFacilityContacts() {
                     <CheckCircleIcon className="h-5 w-5 text-green-400" aria-hidden="true" />
                   </div>
                   <div className="ml-3">
-                    <p className="text-sm font-medium text-green-800">Contact successfully updated</p>
+                    <p className="text-sm font-medium text-green-800">Contact successfully added</p>
                   </div>
                   <div className="ml-auto pl-3">
                     <div className="-mx-1.5 -my-1.5"></div>
