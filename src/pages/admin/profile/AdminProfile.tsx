012243984a0050cd2152627df7e3674f52c9d543
Alex Artemov <artemov.sandro@gmail.com>
2024-02-13 00:28:30 -0500

    Admin Users details phone number enforcing

--- src/pages/admin/profile/AdminProfile.tsx
+++ src/pages/admin/profile/AdminProfile.tsx
@@ -75,24 +75,6 @@ export default function AdminProfile() {
 
               <div className="">
                 <UploadAvatar formUser={formUser} setFormUser={setFormUser} />
-
-                {/* <div className="flex-1 ml-10">
-                  <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
-                    About
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:outline-none focus:ring-green-600 sm:text-sm sm:leading-6"
-                      defaultValue={''}
-                    />
-                  </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-600">Write a few sentences about yourself.</p>
-                </div> */}
-
-                {/* <UploadArea label="Photo" name="photo" path={`users/${formUser._id}/photo`} required={false} /> */}
               </div>
             </div>
             <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
