d2e37b4946b0b53b5974ffd72c5a5f1ddbcf9c1e
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-02-19 01:50:47 -0500

    worked on responsive sidebar, built POC for new AppShell based on TailwindUI #time 7h

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,15 +6,20 @@ import { ITokenInfo } from '../interfaces/services.interfaces'
 interface AuthContextType {
   user: IUser | undefined
   setUser: (user: IUser) => void
+  profilePath: string
+  setProfilePath: (path: string) => void
 }
 
 const AuthContext = createContext<AuthContextType>({
   user: undefined,
   setUser: () => {},
+  profilePath: '',
+  setProfilePath: () => {},
 })
 
 const AuthProvider = ({ children }: any) => {
   const [user, setUser] = useState<IUser>()
+  const [profilePath, setProfilePath] = useState<string>('')
 
   useEffect(() => {
     const ls_data: ITokenInfo = JSON.parse(localStorage.getItem('ht_usr') as any)
@@ -23,7 +28,28 @@ const AuthProvider = ({ children }: any) => {
     }
   }, [])
 
-  return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>
+  useEffect(() => {
+    if (user && user.role) {
+      switch (user.role) {
+        case 'employee':
+          setProfilePath('/employee/profile')
+          break
+        case 'client':
+          setProfilePath('/client/profile')
+          break
+        case 'admin':
+          setProfilePath('/admin/profile')
+          break
+        default:
+          setProfilePath('/')
+          break
+      }
+    } else {
+      setProfilePath('/')
+    }
+  }, [user])
+
+  return <AuthContext.Provider value={{ user, setUser, profilePath, setProfilePath }}>{children}</AuthContext.Provider>
 }
 
 const useAuth = () => useContext(AuthContext)
