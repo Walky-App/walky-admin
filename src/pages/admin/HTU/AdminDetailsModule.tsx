c8c1c35fe22145523edf9d72303ae47129907a36
Jorge Poveda <jpoveda731@gmail.com>
2024-02-19 07:51:14 -0500

    feat: created disable button in admin to modules

--- src/pages/admin/HTU/AdminDetailsModule.tsx
+++ src/pages/admin/HTU/AdminDetailsModule.tsx
@@ -3,12 +3,19 @@ import HeaderComponent from "../../../components/shared/general/HeaderComponent"
 import { useEffect } from "react"
 import FormModule from "./components/FormModule"
 import { useAdmin } from "../../../contexts/AdminContext"
+import { DisableButtonInterface } from "../../../interfaces/Global"
 
 
 
 export default function AdminDetailsModule() {
     const { module, categoryOptions } = useAdmin()
     const navigate = useNavigate()
+
+    const disableButtonData: DisableButtonInterface = {
+        path: `modules/disable/${module?._id}`,
+        status: module?.is_disabled as boolean,
+        redirect: '/admin/learn/modules'
+    }
 
     useEffect(() => {
         if (!module || categoryOptions.length <= 1) {
@@ -19,7 +26,7 @@ export default function AdminDetailsModule() {
 
     return (
         <div className="w-full sm:overflow-x-hidden">
-            <HeaderComponent title={'Update Module'} />
+            <HeaderComponent title={'Update Module'} disableButton={disableButtonData} />
             <FormModule action="edit" module={module} />
         </div>
     )
