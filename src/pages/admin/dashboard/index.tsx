ea3cec9703b9aa20e13677269ddd414f17d2e164
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-02-20 15:25:37 -0500

    worked on how to customize current theme in primereact #time 2h

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,8 +1,32 @@
 import { TaskList, ClassList, CalendarWidget, MessagesWidget } from './widgets'
+import { Button } from 'primereact/button'
+import { Toast } from 'primereact/toast'
+
+import { useRef } from 'react'
 
 export default function AdminDashboard() {
+  const toast = useRef<Toast>(null)
+
+  const handleShow = () => {
+    toast.current?.show({ severity: 'info', summary: 'Info', detail: 'Message Content' })
+  }
+
   return (
     <>
+      <div className="card justify-content-center flex">
+        <Toast ref={toast} />
+        <Button size='small' onClick={handleShow} label="Show" />
+      </div>
+      <br />
+      <div className="card justify-content-center flex flex-wrap gap-3">
+        <Button label="Primary" />
+        <Button label="Secondary" severity="secondary" />
+        <Button label="Success" severity="success" />
+        <Button label="Info" severity="info" />
+        <Button label="Warning" severity="warning" />
+        <Button label="Help" severity="help" />
+        <Button label="Danger" severity="danger" />
+      </div>
       <div className="w-full sm:w-1/2">
         <TaskList />
         <ClassList />
---
+++ src/primereact-themes/README.md
@@ -0,0 +1,5 @@
+# How to customize PrimeReact themes
+
+(Full guide coming soon)
+
+-reference: https://youtu.be/TqpGx9Rdyrw?si=S6qkUtu-rkbhGeAZ
