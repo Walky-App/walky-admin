51b4237cec172a2eab686a1a2f5fd6ee7d4361cb
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-02-21 14:22:16 -0500

    added new images to rotate in the login page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import LoginForm from './LoginForm'
 import SignupForm from './SignupForm'
 import ForgotPasswordForm from './ForgotPasswordForm'
+import { set } from 'react-hook-form'
+
+const ImageRotator: React.FC = () => {
+  const [imageUrl, setImageUrl] = useState<string>('')
+
+  useEffect(() => {
+    const imagesUrl = [
+      '/assets/photos/trim-close-up.jpg',
+      '/assets/photos/trim-team.jpg',
+      '/assets/photos/trim2.jpg',
+      'https://hemptemps.com/wp-content/uploads/2021/05/news-cannabis-growth-1583x791.jpg',
+      'https://hemptemps.com/wp-content/uploads/2023/03/20211019_122700-scaled-2560x1280.jpg',
+      'https://valleyadvocate.com/wp-content/uploads/2017/03/ThinkstockPhotos-515262904-e1490709336307-1024x683.jpg',
+      'https://hemptemps.com/wp-content/uploads/2021/10/blog-head-10-20.png',
+    ]
+    const randomImagesUrl = imagesUrl[Math.floor(Math.random() * imagesUrl.length)]
+    setImageUrl(randomImagesUrl)
+  }, [])
+
+  return (
+    <div className="relative hidden w-full sm:h-96 lg:block lg:h-screen lg:basis-1/2">
+      <img
+        alt="Hemp Temp employees trimming"
+        src={imageUrl}
+        className="absolute inset-0 hidden h-full object-cover sm:block"
+      />
+    </div>
+  )
+}
 
 export default function Auth() {
   const [userForm, setUserForm] = useState('Login')
 
   return (
-    <section className="min-h-screen items-center justify-center flex sm:mb-8 md:mb-0 h-full lg:items-center">
-      <div className='flex flex-col gap-10 md:basis-1/2'>
+    <section className="flex h-full min-h-screen items-center justify-center sm:mb-8 md:mb-0 lg:items-center">
+      <div className="flex flex-col gap-10 md:basis-1/2">
         <div className="w-full">
           <div className="flex justify-center">
             <img src="/assets/logos/logo-horizontal-cropped.png" alt="hemp temps logo" height={300} />
@@ -22,11 +51,16 @@ export default function Auth() {
           {userForm === 'Sign up' && <SignupForm />}
           {userForm === 'Forgot Password' && <ForgotPasswordForm />}
 
-          <div className="flex flex-col justify-center items-center gap-2">
-            {userForm === 'Login' &&  <a className="underline font-medium hover:text-green-700" href="#" onClick={() => setUserForm('Forgot Password')} >
-              Forgot your password?
-            </a>}
-           
+          <div className="flex flex-col items-center justify-center gap-2">
+            {userForm === 'Login' && (
+              <a
+                className="font-medium underline hover:text-green-700"
+                href="#"
+                onClick={() => setUserForm('Forgot Password')}>
+                Forgot your password?
+              </a>
+            )}
+
             <p className="text-sm text-zinc-500">
               {/* {userForm === 'Login' && (
                 <section>
@@ -48,19 +82,11 @@ export default function Auth() {
             </p>
           </div>
         </div>
-        <div className='flex justify-center'>
-          <small className="text-xs text-gray-400  ">
-            v.1.0.0{process.env.NODE_ENV === 'development' ? 'd' : 'p'}
-          </small>
+        <div className="flex justify-center">
+          <small className="text-xs text-gray-400  ">v.1.0.0{process.env.NODE_ENV === 'development' ? 'd' : 'p'}</small>
         </div>
       </div>
-      <div className="relative hidden w-full lg:block sm:h-96 lg:h-screen lg:basis-1/2">
-        <img
-          alt="Hemp Temp employees trimming"
-          src="/assets/photos/trim-close-up.jpg"
-          className="absolute inset-0 object-cover h-full hidden sm:block"
-        />
-      </div>
+      <ImageRotator />
     </section>
   )
 }
