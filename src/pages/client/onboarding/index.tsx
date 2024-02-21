76ccc959678f529ffbd9945877d63b59463a8964
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-02-21 02:07:51 -0500

    worked on Client Onboarding #time 7h
    -implemented Primereact Input components
    
    -worked on ReactHookForm implementation

--- src/pages/client/onboarding/index.tsx
+++ src/pages/client/onboarding/index.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
+import React from 'react'
 import HeaderComponent from '../../../components/shared/general/HeaderComponent'
-import Nav from "./Nav";
-import Step1 from './Step1';
-import Step2 from './Step2';
-import Step3 from './Step3';
-import Step4 from './Step4';
+import Nav from './Nav'
+import Step1 from './Step1'
+import Step2 from './Step2'
+import Step3 from './Step3'
+import Step4 from './Step4'
+import { WelcomeDialog } from './WelcomeDialog'
 
-export default function ClientOnboarding(){
-  const [step, setStep] = React.useState<number>(1);
+export default function ClientOnboarding() {
+  const [step, setStep] = React.useState<number>(1)
 
   console.log(step)
 
   return (
     <div>
-      <HeaderComponent title=' Client Onboarding'/>
+      <HeaderComponent title="Client Onboarding" />
       <Nav step={step} setStep={setStep} />
-      {step === 1 && <Step1 />}
-      {step === 2 && <Step2 />}
-      {step === 3 && <Step3 />}
-      {step === 4 && <Step4 />}
-      {step === 5 && <h1>Step 5</h1>}
+      <div className="mt-10">
+        <WelcomeDialog />
+        {step === 1 && <Step1 />}
+        {step === 2 && <Step2 />}
+        {step === 3 && <Step3 />}
+        {step === 4 && <Step4 />}
+        {step === 5 && <h1>Step 5</h1>}
+      </div>
     </div>
   )
 }
