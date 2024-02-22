c5f90e659905f475d2f30b1335e67dbf00d1e4bb
Alex Artemov <artemov.sandro@gmail.com>
2024-02-22 07:15:11 -0500

    two steps working, third step needs configuration

--- src/pages/client/jobs/addJob/ClientAddJob.tsx
+++ src/pages/client/jobs/addJob/ClientAddJob.tsx
@@ -10,44 +10,33 @@ import { Toast } from 'primereact/toast'
 import { MenuItem } from 'primereact/menuitem'
 
 export default function ClientAddJob() {
-  const [activeIndex, setActiveIndex] = useState<number>(0)
+  const [activeIndex, setActiveIndex] = useState<number>(2)
   const toast = useRef<Toast>(null)
   const [facilities, setFacilities] = React.useState<any>([])
   const [dates, setDates] = useState<Nullable<Date[]>>(null)
   const [jobId, setJobId] = useState<string | null>(null);
 
-  const items: MenuItem[] = [
+  const items = [
     {
-      label: 'Basic Information',
-      command: event => {
-        toast.current?.show({ severity: 'info', summary: 'First Step', detail: event.item.label })
-      },
+        label: 'Basic Information'
     },
     {
-      label: 'Job Details',
-      command: event => {
-        toast.current?.show({ severity: 'info', summary: 'Second Step', detail: event.item.label })
-      },
+        label: 'Job Details'
     },
     {
-      label: 'Shift Details',
-      command: event => {
-        toast.current?.show({ severity: 'info', summary: 'Last Step', detail: event.item.label })
-      },
-    },
-  ]
+        label: 'Shift Details'
+    }
+];
 
   return (
     <>
       <TitleComponent title={'Add a new job'} />
       <div className="card">
-        <Toast ref={toast}></Toast>
-        <Steps model={items} activeIndex={activeIndex} onSelect={e => setActiveIndex(e.index)} readOnly={false} />
+        <Steps model={items} activeIndex={activeIndex}/>
         <div>
-            
-          {activeIndex === 0 && <BasicInfo onJobCreated={setJobId}/>} {/* Only renders this when Step 1 is active */}
-          {activeIndex === 1 && <JobDetails jobId={jobId}/>} {/* Only renders this when Step 2 is active */}
-          {activeIndex === 2 && <ShiftDetails />} {/* Only renders this when Step 3 is active */}
+          {activeIndex === 0 && <BasicInfo onJobCreated={setJobId} onNext={() => setActiveIndex(activeIndex + 1)}/>} {/* Only renders this when Step 1 is active */}
+          {activeIndex === 1 && <JobDetails jobId={jobId} onNext={() => setActiveIndex(activeIndex + 1)} />} {/* Only renders this when Step 2 is active */}
+          {activeIndex === 2 && <ShiftDetails jobId={jobId}/>} {/* Only renders this when Step 3 is active */}
         </div>
       </div>
     </>
