8ce821cc2bf393bd0276945c3e49edac78360ae9
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-02-22 16:32:00 -0500

    randmom images

--- src/pages/admin/dashboard/index.tsx
+++ src/pages/admin/dashboard/index.tsx
@@ -1,12 +1,9 @@
 import { TaskList, ClassList, CalendarWidget, MessagesWidget } from './widgets'
-import ToastComponent from '../../../components/shared/general/ToastComponent'
+
 
 export default function AdminDashboard() {
   return (
     <>
-    <div>
-      <ToastComponent />
-    </div>
       <div className="w-full sm:w-1/2">
         <TaskList />
         <ClassList />
