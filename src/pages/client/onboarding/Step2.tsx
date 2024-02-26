2672d080c4abdf894c7467341418987911eb5d81
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-02-26 16:41:00 -0500

    worked on Client Onboarding #time 8h
    -successfully posting facilities added during Client Onboarding
    -able to edit during the flow
    
    -WIP: upload licenses, only a mock for now

--- src/pages/client/onboarding/Step2.tsx
+++ src/pages/client/onboarding/Step2.tsx
@@ -8,111 +8,59 @@ import {
 import { FormDataContext, StepProps } from '.'
 import { Button } from 'primereact/button'
 import { useState, useRef, useContext } from 'react'
-import { Toast } from 'primereact/toast'
+import { Toast, ToastMessage } from 'primereact/toast'
 import { useAuth } from '../../../contexts/AuthContext'
 import { GetTokenInfo } from '../../../utils/TokenUtils'
 
 export default function Step2({ step, setStep }: StepProps) {
-  const { facilitiesArray, setFacilitiesArray, setEditingIndex } = useContext(FormDataContext)
-  const [uploading, setUploading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
+
+  const { setFacilitiesArray } = useContext(FormDataContext)
 
   const { user } = useAuth()
-  console.log('facilitiesArray: ', facilitiesArray)
-
-  const [file, setFile] = useState<any>([])
-  // console.log('file: ', file)
 
   const toast = useRef<Toast>(null)
-  const fileUploadRef = useRef<FileUpload>(null)
-  console.log('fileUploadRef: ', fileUploadRef.current?.props)
 
-  const [uploadedFiles, setUploadedFiles] = useState([])
-  // console.log('uploadedFiles: ', uploadedFiles)
+  const showSavedToast = () => {
+    setIsLoading(true)
+    // @ts-ignore
+    toast.current?.show({
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Changes saved successfully.',
+      life: 2000,
+    })
+  }
+
+  const onRemove = (toastData: ToastMessage) => {
+    // @ts-ignore
+    const severity = toastData.message ? toastData.message.severity : toastData.severity
+
+    if (severity === 'success') {
+      setStep(step + 1)
+    }
+
+    setIsLoading(false)
+  }
+
+  const fileUploadRef = useRef<FileUpload>(null)
 
   function setHeaders(event: FileUploadBeforeSendEvent) {
     const { access_token } = GetTokenInfo()
     event.xhr.setRequestHeader('Authorization', `Bearer ${access_token}`)
   }
 
-  function uploadComplete(event: FileUploadUploadEvent) {
-    const response = event.xhr.response
-    const files = JSON.parse(response)
-    setUploadedFiles(files)
-  }
-
-  function uploadError(req: FileUploadErrorEvent) {
-    const errMsg = req?.xhr?.response || 'Unable to upload file'
-    console.log('errMsg: ', errMsg)
-  }
-
-  // useEffect(() => {
-  //   const getFacility = async () => {
-  //     try {
-  //       const facilityFound = await RequestService(`facilities/${facilityId}`)
-  //       setFacility(facilityFound)
-  //     } catch (error) {
-  //       console.error('Error fetching facility data:', error)
-  //     }
-  //   }
-  //   getFacility()
-  // }, [facilityId])
-
-  // const customUploadHandler = (event: FileUploadHandlerEvent) => {
-  //   const files = event.files
-  //   const documentType = fileUploadRef.current?.props.id as string
-  //   const facilityIndex = 0 // Example: find the index based on facilityId or other criteria
-
-  //   files.forEach(file => {
-  //     setFacilitiesArray(prevState =>
-  //       prevState.map((facility, index) => {
-  //         if (index !== facilityIndex) {
-  //           return facility
-  //         }
-
-  //         return {
-  //           ...facility,
-  //           [documentType]: {
-  //             key: file.name,
-  //             url: URL.createObjectURL(file),
-  //             timestamp: new Date().toISOString(),
-  //             createdBy: user._id,
-  //           },
-  //         }
-  //       }),
-  //     )
-  //   })
-
-  //   // Signal the upload completion to the FileUpload component
-  //   event.options.props.onUpload() // Assuming `onUpload` is accessible through `event.options`
-  // }
-
-  // const beforeSendHandler = (event: FileUploadBeforeSendEvent) => {
-  //   const { access_token } = GetTokenInfo()
-
-  //   event.xhr.setRequestHeader('Authorization', `Bearer ${access_token}`)
-  // }
-
-  const [selectedFiles, setSelectedFiles] = useState<File[]>([])
-  // console.log('selectedFiles: ', selectedFiles)
-
-  // const handleSelect = (e: FileUploadSelectEvent) => {
-  //   // Update component state or UI
-  //   setSelectedFiles(e.files)
-  // }
-
   const handleSelect = async (event: FileUploadSelectEvent) => {
     const files = event.files
-    console.log('files: ', files)
 
     const documentType = fileUploadRef.current?.props.id as string
-    console.log('documentType: ', documentType)
+
     const facilityIndex = 0 // Replace with the actual index or use the facilityId to find the index
 
     for (const file of files) {
       setFacilitiesArray(prevState =>
         prevState.map((facility, index) => {
           if (index !== facilityIndex) {
-            console.log('returned early')
             return facility
           }
 
@@ -128,17 +76,11 @@ export default function Step2({ step, setStep }: StepProps) {
         }),
       )
     }
-
-    console.log('handleSelect invoked')
-  }
-
-  const onUploadToast = () => {
-    toast.current?.show({ severity: 'info', summary: 'Success', detail: 'File Uploaded' })
   }
 
   return (
     <div>
-      <Toast ref={toast}></Toast>
+      <Toast ref={toast} onRemove={e => onRemove(e)}></Toast>
       <div className="space-y-12">
         <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
           <div>
@@ -155,35 +97,6 @@ export default function Step2({ step, setStep }: StepProps) {
                 *Upload State License:
               </label>
               <div className="mt-2">
-                {/* <FileUpload
-                  ref={fileUploadRef}
-                  // id="stateLicenseDocument"
-                  url="/api/upload"
-                  mode="basic"
-                  accept="image/*"
-                  maxFileSize={1000000}
-                  // onBeforeUpload={}
-                  // onSelect={beforeUploadHandler}
-                  // onBeforeSend={beforeSendHandler}
-                  // uploadHandler={handleImagesUpload}
-                  customUpload
-                  uploadHandler={beforeUploadHandler}
-                  // onUpload={onUploadToast}
-                /> */}
-                {/* <FileUpload
-                  ref={fileUploadRef}
-                  id="stateLicenseDocument"
-                  name="files"
-                  // url={'/api/upload'}
-                  multiple
-                  accept="image/*"
-                  maxFileSize={1000000}
-                  customUpload
-                  uploadHandler={customUploadHandler}
-                  // onSelect={beforeUploadHandler}
-                  // onUpload={}
-                  emptyTemplate={<p className="m-0">Drag and drop files to here to upload.</p>}
-                /> */}
                 <FileUpload
                   id="stateLicenseDocument"
                   name="file"
@@ -230,45 +143,6 @@ export default function Step2({ step, setStep }: StepProps) {
         </div>
       </div>
 
-      <ul role="list" className="divide-y divide-gray-100">
-        {/* {facilitiesArray?.map(({ license }: 
-          { license: { key: string; url: string; timestamp: string; createdBy: string } }
-        ) => (
-          <li key={license._id} className="flex items-center justify-between gap-x-6 py-5">
-            <div className="min-w-0">
-              <div className="flex items-start gap-x-3">
-                <a href={license.url} target="_blank" className="hidden px-5 py-1.5 text-sm font-semibold sm:block">
-                  <p className="text-sm font-semibold leading-6 text-gray-900 hover:text-gray-500">{license.key}</p>
-                </a>
-                <p
-                  className={classNames(
-                    statuses['Complete'],
-                    'mt-0.5 whitespace-nowrap rounded-md px-1.5 py-0.5 text-xs font-medium ring-1 ring-inset',
-                  )}>
-                  Approved
-                </p>
-              </div>
-              <div className="mt-1 flex items-center gap-x-2 text-xs leading-5 text-gray-500">
-                <p className="whitespace-nowrap">
-                  Uploaded on <time dateTime={license.timestamp}>{license.timestamp}</time>
-                </p>
-                <svg viewBox="0 0 2 2" className="h-0.5 w-0.5 fill-current">
-                  <circle cx={1} cy={1} r={1} />
-                </svg>
-                <p className="truncate">Created by {license.createdBy}</p>
-              </div>
-            </div> */}
-        {/* <div className="flex flex-none items-center gap-x-4 disabled">
-              <a
-                href="#"
-                target="_blank"
-                className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block">
-                Delete
-              </a>
-            </div> */}
-        {/* </li> */}
-        {/* ))} */}
-      </ul>
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <Button
           severity="secondary"
@@ -276,10 +150,9 @@ export default function Step2({ step, setStep }: StepProps) {
           outlined
           onClick={() => {
             setStep(step - 1)
-            setEditingIndex(facilitiesArray.length - 1)
           }}
         />
-        <Button label="Save" onClick={() => setStep(step + 1)} />
+        <Button label="Save" onClick={showSavedToast} loading={isLoading} />
       </div>
     </div>
   )
--- src/pages/client/onboarding/Step3.tsx
+++ src/pages/client/onboarding/Step3.tsx
@@ -2,11 +2,10 @@ import { Fragment, useContext, useRef, useState } from 'react'
 import { Menu, Transition } from '@headlessui/react'
 import { EllipsisHorizontalIcon, PhotoIcon } from '@heroicons/react/20/solid'
 import { cn } from '../../../utils/cn'
-import { Toast } from 'primereact/toast'
+import { Toast, ToastMessage } from 'primereact/toast'
 import { Button } from 'primereact/button'
-import { FormDataContext, IFacilityFormInputs, StepProps } from '.'
+import { FormDataContext, StepProps } from '.'
 import AddFacilityDialog from './AddFacilityDialog'
-import { ConfirmDialog, confirmDialog } from 'primereact/confirmdialog'
 
 export function joinTruthyStrings(strings: (string | undefined)[], separator: string): string {
   return strings.filter(Boolean).join(separator)
@@ -14,37 +13,32 @@ export function joinTruthyStrings(strings: (string | undefined)[], separator: st
 
 export default function Step3({ step, setStep }: StepProps) {
   const [visible, setVisible] = useState<boolean>(false)
+  const [isLoading, setIsLoading] = useState(false)
 
-  const { facilitiesArray, setFacilitiesArray, defaultValues, selectedFacility, setSelectedFacility } =
-    useContext(FormDataContext)
-
-  console.log('selectedFacility: ', selectedFacility)
+  const { facilitiesArray, defaultValues, selectedFacility, setSelectedFacility } = useContext(FormDataContext)
 
   const toast = useRef(null)
 
-  const acceptToast = ({ name }: IFacilityFormInputs) => {
+  const showSavedToast = () => {
+    setIsLoading(true)
     // @ts-ignore
     toast.current?.show({
-      severity: 'info',
-      summary: 'Confirmed',
-      detail: `${name} removed`,
-      life: 3000,
+      severity: 'success',
+      summary: 'Success',
+      detail: 'Changes saved successfully.',
+      life: 2000,
     })
   }
 
-  const confirm1 = (facility: IFacilityFormInputs) => {
-    confirmDialog({
-      message: `Are you sure you want to delete ${facility.name} ?`,
-      header: 'Confirmation',
-      icon: 'pi pi-exclamation-triangle',
-      defaultFocus: 'accept',
-      acceptClassName: 'p-button-danger',
-      accept: () => {
-        setFacilitiesArray(facilitiesArray.filter(f => f.name !== facility.name))
-        acceptToast(facility)
-      },
-      rejectClassName: 'p-button-danger p-button-outlined',
-    })
+  const onRemove = (toastData: ToastMessage) => {
+    // @ts-ignore
+    const severity = toastData.message ? toastData.message.severity : toastData.severity
+
+    if (severity === 'success') {
+      setStep(step + 1)
+    }
+
+    setIsLoading(false)
   }
 
   return (
@@ -55,8 +49,8 @@ export default function Step3({ step, setStep }: StepProps) {
         toastRef={toast}
         values={selectedFacility || defaultValues}
       />
-      <ConfirmDialog draggable={false} />
-      <Toast ref={toast} />
+      <Toast ref={toast} onRemove={e => onRemove(e)}></Toast>
+
       {/* Do you have more locations to add?  */}
       <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
         <div>
@@ -69,13 +63,8 @@ export default function Step3({ step, setStep }: StepProps) {
         <div className="sm:col-span-full">
           <ul className="grid auto-rows-fr grid-cols-1 gap-x-6 gap-y-8 lg:grid-cols-3 xl:gap-x-8">
             {facilitiesArray?.map(facility => (
-              <li key={facility.name} className="overflow-hidden rounded-xl border border-gray-200">
+              <li key={facility._id} className="overflow-hidden rounded-xl border border-gray-200">
                 <div className="flex items-center gap-x-4 border-b border-gray-900/5 bg-gray-50 p-6">
-                  {/* <img
-                    src={client.imageUrl}
-                    alt={client.name}
-                    className="h-12 w-12 flex-none rounded-lg bg-white object-cover ring-1 ring-gray-900/10"
-                  /> */}
                   <PhotoIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
                   <div className="text-sm font-medium leading-6 text-gray-900">{facility.name}</div>
                   <Menu as="div" className="relative ml-auto">
@@ -92,17 +81,6 @@ export default function Step3({ step, setStep }: StepProps) {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95">
                       <Menu.Items className="absolute right-0 z-10 mt-0.5 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                        {/* <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              className={cn(
-                                active ? 'bg-gray-50' : '',
-                                'block px-3 py-1 text-sm leading-6 text-gray-900',
-                              )}>
-                              View<span className="sr-only">, {facility.name}</span>
-                            </button>
-                          )}
-                        </Menu.Item> */}
                         <Menu.Item>
                           {({ active }) => (
                             <button
@@ -112,21 +90,9 @@ export default function Step3({ step, setStep }: StepProps) {
                               }}
                               className={cn(
                                 active ? 'bg-gray-50' : '',
-                                'block px-3 py-1 text-sm leading-6 text-gray-900',
+                                'block w-full px-3 py-1 text-start text-sm leading-6 text-gray-900',
                               )}>
                               Edit<span className="sr-only">, {facility.name}</span>
-                            </button>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <button
-                              onClick={() => confirm1(facility)}
-                              className={cn(
-                                active ? 'bg-gray-50' : '',
-                                'block px-3 py-1 text-sm leading-6 text-gray-900',
-                              )}>
-                              Delete<span className="sr-only">, {facility.name}</span>
                             </button>
                           )}
                         </Menu.Item>
@@ -135,12 +101,6 @@ export default function Step3({ step, setStep }: StepProps) {
                   </Menu>
                 </div>
                 <dl className="-my-3 divide-y divide-gray-100 px-6 py-4 text-sm leading-6">
-                  {/* <div className="flex justify-between gap-x-4 py-3">
-                    <dt className="text-gray-500">Last Added</dt>
-                    <dd className="text-gray-700">
-                      <time dateTime="2022-12-13">Feb 21, 2024</time>
-                    </dd>
-                  </div> */}
                   <div className="flex justify-between gap-x-4 py-3">
                     <dt className="text-gray-500">Address</dt>
                     <dd className="flex items-start gap-x-2">
@@ -150,11 +110,17 @@ export default function Step3({ step, setStep }: StepProps) {
                     </dd>
                   </div>
                   <div className="flex justify-between gap-x-4 py-3">
-                    <dt className="text-gray-500">{facility.contacts[0].role}</dt>
+                    <dt className="text-gray-500">Contact name</dt>
                     <dd className="flex items-start gap-x-2">
                       <div className="font-medium text-gray-900">
                         {joinTruthyStrings([facility.contacts[0].first_name, facility.contacts[0].last_name], ' ')}
                       </div>
+                    </dd>
+                  </div>
+                  <div className="flex justify-between gap-x-4 py-3">
+                    <dt className="text-gray-500">Role</dt>
+                    <dd className="flex items-start gap-x-2">
+                      <div className="font-medium text-gray-900">{facility.contacts[0].role}</div>
                     </dd>
                   </div>
                 </dl>
@@ -180,7 +146,7 @@ export default function Step3({ step, setStep }: StepProps) {
       </div>
       <div className="mt-6 flex items-center justify-end gap-x-6">
         <Button severity="secondary" label="Back" outlined onClick={() => setStep(step - 1)} />
-        <Button label="Save" onClick={() => setStep(step + 1)} />
+        <Button label="Save" onClick={showSavedToast} loading={isLoading} />
       </div>
     </div>
   )
