186b617d9bada525031cd728a00408673b8510ff
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-02-26 12:46:21 -0500

    Added the login show password

--- src/pages/auth/LoginForm.tsx
+++ src/pages/auth/LoginForm.tsx
@@ -99,6 +99,21 @@ export default function LoginForm() {
             className="w-full rounded-lg border-zinc-200 p-4 pe-12 shadow-sm  focus:border-green-500 focus:ring-green-500"
             placeholder="*Email"
           />
+          <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-4 w-4 text-zinc-700"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207"
+              />
+            </svg>
+          </span>
         </div>
       </div>
 
