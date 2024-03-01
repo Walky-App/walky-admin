46de8a24dab7c9fb42738a20052e179e39ff5637
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-02-29 22:29:45 -0500

    worked on eslint typescript and react rules setup #time 5h

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
 import eslint from '@eslint/js'
 import prettierConfig from 'eslint-config-prettier'
+import fileNameRules from 'eslint-plugin-filename-rules'
+import importPlugin from 'eslint-plugin-import'
+import jsxA11y from 'eslint-plugin-jsx-a11y'
+import prettierPlugin from 'eslint-plugin-prettier'
 import react from 'eslint-plugin-react'
+import reactHooks from 'eslint-plugin-react-hooks'
 import reactRecommended from 'eslint-plugin-react/configs/recommended.js'
 import globals from 'globals'
 import { dirname } from 'path'
@@ -15,11 +20,20 @@ export default tseslint.config(
   ...tseslint.configs.recommendedTypeChecked,
   ...tseslint.configs.stylisticTypeChecked,
   {
-    files: ['**/*.{js,jsx,mjs,cjs,ts,tsx}'],
     ...reactRecommended,
-    prettierConfig,
+    ...prettierConfig,
     plugins: {
       react,
+      import: importPlugin,
+      prettier: prettierPlugin,
+      'react-hooks': reactHooks,
+      'jsx-a11y': jsxA11y,
+      'filename-rules': fileNameRules,
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
     languageOptions: {
       parserOptions: {
@@ -35,9 +49,73 @@ export default tseslint.config(
       },
     },
     rules: {
+      ...reactHooks.configs.recommended.rules,
+      ...jsxA11y.configs.recommended.rules,
+      'prettier/prettier': 'off',
       'no-console': ['error', { allow: ['warn', 'error'] }],
+      'react/jsx-uses-react': 'error',
+      'react/prefer-stateless-function': 'error',
+      'react/button-has-type': 'error',
+      'react/no-unused-prop-types': 'error',
+      'react/jsx-pascal-case': 'error',
+      'react/jsx-no-script-url': 'error',
+      'react/no-children-prop': 'error',
+      'react/no-danger': 'error',
+      'react/no-danger-with-children': 'error',
+      'react/no-unstable-nested-components': ['error', { allowAsProps: true }],
+      'react/jsx-fragments': ['error', 'syntax'],
+      'react/destructuring-assignment': ['error', 'always', { destructureInSignature: 'always' }],
+      'react/jsx-no-leaked-render': ['error', { validStrategies: ['ternary'] }],
+      'react/jsx-max-depth': ['error', { max: 5 }],
+      'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
+      'react/jsx-key': [
+        'error',
+        {
+          checkFragmentShorthand: true,
+          checkKeyMustBeforeSpread: true,
+          warnOnDuplicates: true,
+        },
+      ],
+      'react/jsx-no-useless-fragment': 'warn',
+      'react/jsx-curly-brace-presence': 'warn',
+      'react/no-typos': 'warn',
+      'react/display-name': 'warn',
+      'react/self-closing-comp': 'warn',
+      'react/jsx-sort-props': 'warn',
       'react/react-in-jsx-scope': 'off',
-      'react/jsx-uses-react': 'off',
+      'react/jsx-one-expression-per-line': 'off',
+      'react/prop-types': 'off',
+      'react/no-array-index-key': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'default',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'variable',
+          format: ['PascalCase', 'camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'parameter',
+          format: ['camelCase'],
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'property',
+          format: null,
+          leadingUnderscore: 'allow',
+        },
+        {
+          selector: 'typeLike',
+          format: ['PascalCase'],
+        },
+      ],
+      'filename-rules/match': [2, { '.ts': 'camelcase', '.tsx': 'pascalcase' }],
+      'import/no-default-export': 'warn',
     },
   },
 )
