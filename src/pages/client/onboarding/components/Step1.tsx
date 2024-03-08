84a75306a7a265030c39e51e41699e0765a76c61
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-03-07 23:32:05 -0500

    worked on image upload for Client Onboarding #time 10h
    - cleaned up unused code
    - fixed variable names
    - removed unused component (DialogModal)

--- src/pages/client/onboarding/components/Step1.tsx
+++ src/pages/client/onboarding/components/Step1.tsx
@@ -1,14 +1,26 @@
-import { useContext, useRef, useState } from 'react'
+import { useContext, useEffect, useRef, useState } from 'react'
 import { Controller, SubmitHandler, useFieldArray, useForm } from 'react-hook-form'
 import { Button } from 'primereact/button'
 import { Dropdown } from 'primereact/dropdown'
+import {
+  FileUpload,
+  FileUploadBeforeSendEvent,
+  FileUploadErrorEvent,
+  FileUploadSelectEvent,
+  FileUploadUploadEvent,
+} from 'primereact/fileupload'
+import { Image } from 'primereact/image'
 import { InputMask } from 'primereact/inputmask'
 import { InputNumber } from 'primereact/inputnumber'
 import { InputText } from 'primereact/inputtext'
+import { InputTextarea } from 'primereact/inputtextarea'
+import { MultiSelect, MultiSelectChangeEvent } from 'primereact/multiselect'
+import { Panel } from 'primereact/panel'
 import { Toast, ToastMessage } from 'primereact/toast'
 import { classNames } from 'primereact/utils'
 
 import { RequestService } from '../../../../services/RequestService'
+import { GetTokenInfo } from '../../../../utils/TokenUtils'
 import {
   FormDataContext,
   getFormErrorMessage,
@@ -18,11 +30,27 @@ import {
 } from '../ClientOnboardingPage'
 import { countries, states } from '../formOptions'
 
+const services = ['Trimming', 'Harvesting', 'Packaging', 'Budtending', 'Gardening', 'General Labor', 'Other']
+
 export const Step1 = ({ step, setStep }: StepProps) => {
+  const [showFileUploader, setShowFileUploader] = useState(true)
+
+  const [selectedImages, setSelectedImages] = useState<File[]>([])
+
   const [isLoading, setIsLoading] = useState(false)
   const { setFormData, defaultValues, formData, facilitiesArray, setFacilitiesArray } = useContext(FormDataContext)
 
   const toast = useRef(null)
+  const fileUploadRef = useRef<FileUpload>(null)
+  console.log('fileUploadRef: ', fileUploadRef)
+  const fileUploadUrlRef = useRef('')
+  console.log('fileUploadUrlRef: ', fileUploadUrlRef)
+  const uploadErrorRef = useRef(false)
+  console.log('uploadErrorRef: ', uploadErrorRef)
+
+  useEffect(() => {
+    setShowFileUploader(facilitiesArray[0]?.images.length > 2 ? false : true)
+  }, [facilitiesArray])
 
   const onRemoveToast = (toastData: ToastMessage) => {
     // @ts-expect-error toastRef.current may be null
@@ -51,28 +79,51 @@ export const Step1 = ({ step, setStep }: StepProps) => {
 
   const onSubmit: SubmitHandler<IFacilityFormInputs> = async data => {
     setFormData(data)
-    const facilityId = facilitiesArray[0]?._id
-
     setIsLoading(true)
-    if (facilityId) {
+
+    const existingFacilityId = facilitiesArray[0]?._id
+
+    const uploadFiles = (facilityId: string): void => {
+      if (selectedImages && selectedImages.length > 0) {
+        fileUploadUrlRef.current = `${process.env.REACT_APP_PUBLIC_API}/facilities/${facilityId}/images`
+
+        fileUploadRef.current?.upload()
+      }
+    }
+
+    let facilityId
+
+    if (existingFacilityId) {
+      // Existing facility, PATCH operation
       try {
-        const facilityFound = await RequestService(`facilities/${facilityId}`)
+        const facilityFound = await RequestService(`facilities/${existingFacilityId}`)
 
         if (facilityFound) {
-          const updatedFacility = { ...facilityFound, ...data, licenses: facilitiesArray[0].licenses }
+          const updatedFacility = {
+            ...facilityFound,
+            ...data,
+            licenses: facilitiesArray[0].licenses,
+            images: facilitiesArray[0].images,
+          }
 
-          const response = await RequestService(`facilities/${facilityId}`, 'PATCH', updatedFacility)
+          const response = await RequestService(`facilities/${existingFacilityId}`, 'PATCH', updatedFacility)
 
           if (response?._id) {
-            // @ts-expect-error toastRef.current may be null
-            toast.current?.show({
-              severity: 'success',
-              summary: 'Changes saved for:',
-              detail: getValues('name'),
-            })
-            setFacilitiesArray(prevArray =>
-              prevArray.map(facility => (facility._id === response._id ? response : facility)),
-            )
+            facilityId = response._id
+
+            uploadFiles(facilityId)
+
+            if (uploadErrorRef.current) {
+              // @ts-expect-error toastRef.current may be null
+              toast.current?.show({
+                severity: 'success',
+                summary: 'Changes saved for:',
+                detail: getValues('name'),
+              })
+              setFacilitiesArray(prevArray =>
+                prevArray.map(facility => (facility._id === response._id ? response : facility)),
+              )
+            }
           } else {
             throw new Error('Failed to update facility')
           }
@@ -89,18 +140,25 @@ export const Step1 = ({ step, setStep }: StepProps) => {
         })
       }
     } else {
+      // New facility, POST operation
       try {
         const response = await RequestService(`facilities`, 'POST', data)
 
         if (response?._id) {
-          // @ts-expect-error toastRef.current may be null
-          toast.current?.show({
-            severity: 'success',
-            summary: 'Success',
-            detail: `${getValues('name')} created successfully.`,
-            life: 2000,
-          })
-          setFacilitiesArray([response])
+          facilityId = response._id
+
+          uploadFiles(facilityId)
+
+          if (uploadErrorRef.current) {
+            // @ts-expect-error toastRef.current may be null
+            toast.current?.show({
+              severity: 'success',
+              summary: 'Success',
+              detail: `${getValues('name')} created successfully.`,
+              life: 2000,
+            })
+            setFacilitiesArray([response])
+          }
         } else {
           throw new Error('Failed to add facility')
         }
@@ -113,6 +171,47 @@ export const Step1 = ({ step, setStep }: StepProps) => {
           detail: `${getValues('name')} already exists.`,
         })
       }
+    }
+  }
+
+  const handleFileSelect = (event: FileUploadSelectEvent) => {
+    const files = event.files
+    setSelectedImages(prevFiles => [...prevFiles, ...files])
+
+    console.log('files: ', files)
+  }
+
+  const handleBeforeSend = (event: FileUploadBeforeSendEvent) => {
+    const { access_token } = GetTokenInfo()
+    event.xhr.setRequestHeader('Authorization', `Bearer ${access_token}`)
+  }
+
+  const handleUploadError = (event: FileUploadErrorEvent) => {
+    uploadErrorRef.current = true
+    console.error('Error uploading file:', event.files[0]?.name)
+    // @ts-expect-error toastRef.current may be null
+    toast.current?.show({
+      severity: 'error',
+      summary: 'Error',
+      detail: `Error uploading ${event.files[0]?.name}`,
+      life: 2000,
+    })
+  }
+
+  const handleUploadSuccess = (event: FileUploadUploadEvent) => {
+    if (event.xhr.status === 200) {
+      const data: IFacilityFormInputs = JSON.parse(event.xhr.response)
+      // @ts-expect-error toastRef.current may be null
+      toast.current?.show({
+        severity: 'info',
+        summary: 'File Uploaded',
+        detail: `${event.files[0].name} has been uploaded successfully.`,
+        life: 2000,
+      })
+    } else {
+      console.error('Error status:', event.xhr.status)
+      console.error('Error status text:', event.xhr.statusText)
+      console.error('Error response text:', event.xhr.responseText)
     }
   }
 
@@ -251,7 +350,7 @@ export const Step1 = ({ step, setStep }: StepProps) => {
             </div>
 
             <div className="sm:col-span-3">
-              <label htmlFor="sqft" className="block text-sm font-medium leading-6 text-gray-900">
+              <label htmlFor="notes" className="block text-sm font-medium leading-6 text-gray-900">
                 *Facility Square Footage:
               </label>
               <div className="mt-2">
@@ -279,6 +378,55 @@ export const Step1 = ({ step, setStep }: StepProps) => {
                 />
               </div>
               {getFormErrorMessage('sqft', errors)}
+            </div>
+
+            <div className="sm:col-span-6">
+              <label htmlFor="services" className="block text-sm font-medium leading-6 text-gray-900">
+                *Services:
+              </label>
+              <div className="mt-2">
+                <Controller
+                  control={control}
+                  name="services"
+                  rules={{ required: 'At least one Serivce is required' }}
+                  render={({ field, fieldState }) => (
+                    <MultiSelect
+                      id={field.name}
+                      {...field}
+                      value={field.value}
+                      options={services}
+                      display="chip"
+                      onChange={(e: MultiSelectChangeEvent) => field.onChange(e.value)}
+                      placeholder="Select Services"
+                      className={classNames({ 'p-invalid': fieldState.invalid })}
+                    />
+                  )}
+                />
+                {getFormErrorMessage('services', errors)}
+              </div>
+            </div>
+
+            <div className="sm:col-span-6">
+              <label htmlFor="sqft" className="block text-sm font-medium leading-6 text-gray-900">
+                Facility notes:
+              </label>
+              <div className="mt-2">
+                <Controller
+                  control={control}
+                  name="notes"
+                  rules={{ required: false }}
+                  render={({ field, fieldState }) => (
+                    <InputTextarea
+                      id={field.name}
+                      {...field}
+                      rows={4}
+                      cols={30}
+                      className={classNames({ 'p-invalid': fieldState.invalid })}
+                    />
+                  )}
+                />
+              </div>
+              {getFormErrorMessage('notes', errors)}
             </div>
           </div>
         </div>
@@ -539,6 +687,82 @@ export const Step1 = ({ step, setStep }: StepProps) => {
               </div>
             </div>
           ))}
+        </div>
+
+        {/* Facility Images */}
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Facility Images</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">Upload any images of your facility.</p>
+          </div>
+
+          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6 md:col-span-2">
+            {facilitiesArray[0]?.images.length > 0 ? (
+              <div className="sm:col-span-6">
+                <Panel header="Uploaded Images">
+                  <div className="flex flex-wrap items-center gap-x-3 gap-y-3">
+                    {facilitiesArray[0]?.images.map((image, index) => {
+                      const fileName = image.key.split('/').pop()
+                      return (
+                        <div key={image.timestamp} className="flex w-full flex-col items-center justify-center">
+                          <Image src={image.url} alt={fileName} preview pt={{ image: { className: 'h-16 w-auto' } }} />
+                          <p className="cursor-default px-5 py-1.5 text-sm font-semibold leading-6 text-gray-900 hover:text-gray-500">
+                            {fileName}
+                          </p>
+                        </div>
+                      )
+                    })}
+                  </div>
+                </Panel>
+                {!showFileUploader ? (
+                  <Button
+                    className="mt-3"
+                    label="Upload more"
+                    size="small"
+                    onClick={() => setShowFileUploader(true)}
+                    icon="pi pi-plus"
+                  />
+                ) : null}
+              </div>
+            ) : null}
+
+            {showFileUploader ? (
+              <div className="sm:col-span-6">
+                <label htmlFor="stateLicenseDocument" className="block text-sm font-medium leading-6 text-gray-900">
+                  Upload Images:
+                </label>
+                <div className="mt-2">
+                  <FileUpload
+                    id="facilityImages"
+                    name="files"
+                    ref={fileUploadRef}
+                    maxFileSize={10000000}
+                    accept="image/*"
+                    multiple={true}
+                    mode="advanced"
+                    url={fileUploadUrlRef.current}
+                    onSelect={handleFileSelect}
+                    onBeforeSend={handleBeforeSend}
+                    onError={handleUploadError}
+                    onUpload={handleUploadSuccess}
+                    emptyTemplate={
+                      <p>
+                        Drag and drop <u>Facility Image</u> files to upload. Max size: 10MB
+                      </p>
+                    }
+                    previewWidth={200}
+                    pt={{
+                      uploadButton: {
+                        root: {
+                          className: 'hidden',
+                        },
+                      },
+                    }}
+                  />
+                </div>
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
 
