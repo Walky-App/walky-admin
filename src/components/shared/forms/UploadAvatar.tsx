244ef10cbd14616085568c90dfb969b7d84bc29e
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-03-13 00:33:51 -0400

    fix: addressed build issue and errors #time 2.5h
    - fixed build issue
    - fixed react errors from console
    - fixed AdminProfile form error handling
    - fixed AdminProfile markup and styling
    - addressed typing and side effect handling issues in UploadAvatar
    - addressed performance issues related to component rendering in AppShell
    
    -

--- src/components/shared/forms/UploadAvatar.tsx
+++ src/components/shared/forms/UploadAvatar.tsx
@@ -1,40 +1,37 @@
-import { useRef, useState, useMemo } from 'react'
-import { UserCircleIcon, PencilIcon } from '@heroicons/react/24/solid'
+import { useRef, useState, useEffect } from 'react'
+
 import { Spinner } from 'flowbite-react'
-import { useAuth } from '../../../contexts/AuthContext'
+
+import { UserCircleIcon, PencilIcon } from '@heroicons/react/24/solid'
+
 import { RequestService } from '../../../services/RequestService'
 
-export default function UploadAvatar({ formUser, setFormUser }: any) {
-  const [file, setFile] = useState<any>()
-  const [previewUrl, setPreviewUrl] = useState<any>(formUser.avatar)
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const UploadAvatar = ({ formUser, setFormUser }: any) => {
+  const [file, setFile] = useState<File | null>(null)
+  const [previewUrl, setPreviewUrl] = useState<string>(formUser.avatar || '')
   const [uploading, setUploading] = useState(false)
-  const { user } = useAuth()
 
-  const filePickerRef = useRef<any>()
+  const filePickerRef = useRef<HTMLInputElement>(null)
 
-  const handleAvatarUpload = async () => {
-    setUploading(true)
-    const formData = new FormData()
-    formData.append('avatar', file)
-
-    const avatar_url = await RequestService(
-      `users/upload-avatar/${formUser._id}`,
-      'POST',
-      formData,
-      'binary',
-    )
-
-    setFormUser({ ...formUser, avatar: avatar_url })
-    setPreviewUrl(avatar_url)
-    setUploading(false)
-  }
-
-  useMemo(() => {
+  useEffect(() => {
     if (!file) return
-    handleAvatarUpload()
-  }, [file])
+    const handleAvatarUpload = async () => {
+      setUploading(true)
+      const formData = new FormData()
+      formData.append('avatar', file)
 
-  const pickedHandler = (event: any) => {
+      const avatar_url = await RequestService(`users/upload-avatar/${formUser._id}`, 'POST', formData, 'binary')
+
+      setFormUser({ ...formUser, avatar: avatar_url })
+      setPreviewUrl(avatar_url)
+      setUploading(false)
+    }
+
+    handleAvatarUpload()
+  }, [file, formUser, setFormUser])
+
+  const pickedHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length === 1) {
       const pickedFile = event.target.files[0]
       setFile(pickedFile)
@@ -42,6 +39,7 @@ export default function UploadAvatar({ formUser, setFormUser }: any) {
   }
 
   const pickImageHandler = () => {
+    // @ts-expect-error filePickerRef.current may be null
     filePickerRef.current.click()
   }
 
@@ -59,8 +57,9 @@ export default function UploadAvatar({ formUser, setFormUser }: any) {
         />
 
         {!uploading ? (
-          <span
-            className="relative inline-block hover:bg-gray-500 rounded-full hover:ring-2 hover:ring-green-500 hover:cursor-pointer"
+          <button
+            type="button"
+            className="relative inline-block rounded-full hover:cursor-pointer hover:bg-gray-500 hover:ring-2 hover:ring-green-500"
             onClick={pickImageHandler}>
             {previewUrl ? (
               <img
@@ -71,10 +70,10 @@ export default function UploadAvatar({ formUser, setFormUser }: any) {
             ) : (
               <UserCircleIcon className="h-20 w-20 text-gray-300" aria-hidden="true" />
             )}
-            <span className="absolute right-0  top-0 block  rounded-xl bg-zinc-200 ring-4 ring-white text-center">
-              <PencilIcon className="h-6 w-6 text-zinc-500 py-1 " aria-hidden="true" />
+            <span className="absolute right-0  top-0 block  rounded-xl bg-zinc-200 text-center ring-4 ring-white">
+              <PencilIcon className="h-6 w-6 py-1 text-zinc-500 " aria-hidden="true" />
             </span>
-          </span>
+          </button>
         ) : (
           <Spinner color="success" size="lg" aria-label="Success spinner example" />
         )}
