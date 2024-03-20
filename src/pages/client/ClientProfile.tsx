afc7cba2e887bd08bfc0c91faf949cc0c419bdb6
Andy Arias <146486695+andyarias30@users.noreply.github.com>
2024-03-20 16:47:43 -0400

    fix type keywords

--- src/pages/client/ClientProfile.tsx
+++ src/pages/client/ClientProfile.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState, useRef } from 'react'
 
-import { Controller, FieldErrors, SubmitHandler, useForm } from 'react-hook-form'
+import { Controller, type FieldErrors, type SubmitHandler, useForm } from 'react-hook-form'
 
 import { Button } from 'primereact/button'
 import { Calendar } from 'primereact/calendar'
