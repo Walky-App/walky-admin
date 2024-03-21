5b9cf0cbfe2adefab3b1896d58ef9daf715ce713
jonathans199 <jonathans199@gmail.com>
2024-03-21 13:12:55 -0400

    added button to generate new polygon for map

--- src/pages/admin/facilities/components/PolygonMap.tsx
+++ src/pages/admin/facilities/components/PolygonMap.tsx
@@ -1,5 +1,7 @@
 import { useRef, useCallback } from 'react'
 
+import { Button } from 'primereact/button'
+
 import { GoogleMap, LoadScript, Polygon } from '@react-google-maps/api'
 
 export const PolygonMap = ({
@@ -16,6 +18,16 @@ export const PolygonMap = ({
   const currentpolygonCoords = locationPolygon.map(coord => ({ lat: coord[0], lng: coord[1] }))
 
   const listenersRef = useRef([])
+
+  const generateNewPolygon = () => {
+    const offset = 0.0001
+    const polygonPointOne: [number, number] = [locationPin[0] - offset, locationPin[1] - offset]
+    const polygonPointTwo: [number, number] = [locationPin[0] - offset, locationPin[1] + offset]
+    const polygonPointThree: [number, number] = [locationPin[0] + offset, locationPin[1] + offset]
+    const polygonPointFour: [number, number] = [locationPin[0] + offset, locationPin[1] - offset]
+    const newPolygon: [number, number][] = [polygonPointOne, polygonPointTwo, polygonPointThree, polygonPointFour]
+    setLocationPolygon(newPolygon)
+  }
 
   const onEdit = useCallback(() => {
     if (polygonRef.current) {
@@ -54,28 +66,34 @@ export const PolygonMap = ({
   const center = { lat: locationPin[0], lng: locationPin[1] }
 
   return (
-    <LoadScript googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_API_KEY}`}>
-      <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={19}>
-        <Polygon
-          path={currentpolygonCoords}
-          options={{
-            editable: true,
-            fillColor: '#FF0000',
-            fillOpacity: 0.35,
-            strokeColor: '#FF0000',
-            strokeOpacity: 0.6,
-            strokeWeight: 2,
-            clickable: true,
-            draggable: false,
-            geodesic: true,
-            zIndex: 1,
-          }}
-          onMouseUp={onEdit}
-          onUnmount={onUnmount}
-          onDragEnd={onEdit}
-          onLoad={onLoad}
-        />
-      </GoogleMap>
-    </LoadScript>
+    <>
+      <Button type="button" onClick={generateNewPolygon}>
+        Generate New Polygon
+      </Button>
+      <br />
+      <LoadScript googleMapsApiKey={`${process.env.REACT_APP_GOOGLE_API_KEY}`}>
+        <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={19}>
+          <Polygon
+            path={currentpolygonCoords}
+            options={{
+              editable: true,
+              fillColor: '#FF0000',
+              fillOpacity: 0.35,
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.6,
+              strokeWeight: 2,
+              clickable: true,
+              draggable: false,
+              geodesic: true,
+              zIndex: 1,
+            }}
+            onMouseUp={onEdit}
+            onUnmount={onUnmount}
+            onDragEnd={onEdit}
+            onLoad={onLoad}
+          />
+        </GoogleMap>
+      </LoadScript>
+    </>
   )
 }
