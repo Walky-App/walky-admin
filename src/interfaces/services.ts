363c3a110fd3fd9e50c253689bae6465d795531b
jonathans199 <jonathans199@gmail.com>
2024-03-25 15:11:57 -0400

    updated lint rules

--- src/interfaces/services.ts
+++ src/interfaces/services.ts
@@ -1,4 +1,6 @@
-export type IRequestService = (path: string, method?: string, body?: never, dataType?: string) => Promise<never>
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+export type IRequestService = (path: string, method?: string, body?: any, dataType?: string) => Promise<any>
 
 export interface ITokenInfo {
   _id: string
