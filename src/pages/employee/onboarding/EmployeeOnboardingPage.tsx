e3f4beb97939cef8892d3186e2ee580dcb408e70
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-03-26 20:17:04 -0400

    fixed responsive styling for Steps component
    - default styling differs with the material theme from primereact

--- src/pages/employee/onboarding/EmployeeOnboardingPage.tsx
+++ src/pages/employee/onboarding/EmployeeOnboardingPage.tsx
@@ -199,7 +199,7 @@ export const EmployeeOnboarding = () => {
         onSelect={e => setActiveIndex(e.index)}
         readOnly={true}
         pt={{
-          label: { className: 'hidden sm:inline' },
+          label: { className: 'hidden xl:inline' },
           menuitem: { className: 'before:top-full before:sm:top-1/2' },
         }}
       />
