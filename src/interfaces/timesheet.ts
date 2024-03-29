39b134b5539c8a4f290896f8547fc0367e4b0fee
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-03-29 05:16:14 -0400

    feat: employee clock-in / clock-out and timesheet

--- src/interfaces/timesheet.ts
+++ src/interfaces/timesheet.ts
@@ -1,15 +1,16 @@
-export interface IPunches {
+export interface IPunch {
   punch_in: boolean
   location: number[]
   in_range: boolean
   is_success: boolean
-  time_stamp: Date
+  time_stamp: string
 }
 
 export interface ITimeSheet {
+  _id: string
   employee_id: string
   job_id: string
   facility_id: string
   is_clocked_in: boolean
-  punches: IPunches[]
+  punches: IPunch[]
 }
