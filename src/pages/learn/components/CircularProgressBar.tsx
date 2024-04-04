35824fd6bf7654b35697cd560b713f9e715a18b6
Jorge Poveda <jpoveda731@gmail.com>
2024-04-04 14:17:15 -0500

    chore: update progress bar

--- src/pages/learn/components/CircularProgressBar.tsx
+++ src/pages/learn/components/CircularProgressBar.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 
 interface ProgressData {
   progressData: {
@@ -7,16 +7,15 @@ interface ProgressData {
   }
 }
 
-
 // #TODO changed to primereact progressbar
 export const CircularProgressBar = ({ progressData }: ProgressData) => {
-  const [currentSkill] = useState(progressData)
-  const percent = Math.floor((currentSkill.complete / currentSkill.total) * 100)
+  const percent = Math.floor((progressData.complete / progressData.total) * 100)
 
   const circumference = 2 * (22 / 7) * 30
   const offset = circumference - (percent / 100) * circumference
 
   useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const svgText: any = document.getElementById('svgText') as HTMLElement
     const textBounding = svgText?.getBBox()
     const textWidth = textBounding?.width || 0
@@ -28,7 +27,7 @@ export const CircularProgressBar = ({ progressData }: ProgressData) => {
   }, [percent, offset])
 
   return (
-    <svg className="transform w-24 h-full">
+    <svg className="h-full w-24 transform">
       <circle
         className="text-gray-300"
         cx="48"
@@ -50,15 +49,8 @@ export const CircularProgressBar = ({ progressData }: ProgressData) => {
         strokeWidth="5"
         transform="rotate(-90 48 48)"
       />
-      <text
-        fill="black"
-        fontSize="0.75em"
-        id="svgText"
-        textAnchor="middle"
-        x="48"
-        y="52"
-      >
-        {`${currentSkill.complete} / ${currentSkill.total}`}
+      <text fill="black" fontSize="0.75em" id="svgText" textAnchor="middle" x="48" y="52">
+        {`${progressData.complete} / ${progressData.total}`}
       </text>
     </svg>
   )
