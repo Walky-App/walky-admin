d629f9176f99d2311d1d4519ff4684d761aacddf
jonathans199 <jonathans199@gmail.com>
2024-04-05 12:32:27 -0400

    communicating with ws

--- src/components/shared/dashboard/MessagesTable.tsx
+++ src/components/shared/dashboard/MessagesTable.tsx
@@ -1,10 +1,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
+
+/* eslint-disable */
 import { useEffect, useState } from 'react'
+
+import { set } from 'react-hook-form'
 
 import { Button } from 'primereact/button'
 import { Column } from 'primereact/column'
 import { DataTable } from 'primereact/datatable'
 import { InputSwitch } from 'primereact/inputswitch'
+import { io, type Socket } from 'socket.io-client'
 
 import { type ITransaction } from '../../../pages/admin/dashboard/AdminDashboard'
 import { RequestService } from '../../../services/RequestService'
@@ -34,6 +39,8 @@ export const MessagesTable: React.FC<Props> = ({ transactions }) => {
   const [selectedProduct, setSelectedProduct] = useState(null)
   const [metaKey, setMetaKey] = useState(true)
 
+  const [socket, setSocket] = useState<Socket | null>(null)
+
   const [messages, setMessages] = useState<IMessage[] | null>([])
   useEffect(() => {
     const getAllMessages = async () => {
@@ -44,8 +51,42 @@ export const MessagesTable: React.FC<Props> = ({ transactions }) => {
     getAllMessages()
   }, [])
 
+  const sendMessage = async () => {
+    if (socket) {
+      socket.emit('send-message', {
+        channel_id: '660ec8eff5dbd8c67a5bac55',
+        user_id: '65b2adf55b4bf7bf54d111f6',
+        message: 'Hello',
+      })
+    }
+  }
+
+  useEffect(() => {
+    const newSocket = io('ws://localhost:4040' as string)
+
+    newSocket.on('connect', () => {
+      newSocket.emit('join-chat', { channel_id: '660ec8eff5dbd8c67a5bac55', user_id: '65b2adf55b4bf7bf54d111f6' })
+    })
+
+    newSocket.on('660ec8eff5dbd8c67a5bac55', () => {
+      // console.log('message received')
+    })
+
+    newSocket.on('disconnect', () => {
+      console.log('disconnect socket')
+    })
+
+    setSocket(newSocket)
+    return () => {
+      newSocket.disconnect()
+    }
+  }, [])
+
   return (
     <div className="card">
+      <button type="button" onClick={sendMessage}>
+        Send Message
+      </button>
       <DataTable
         value={products}
         selectionMode="single"
