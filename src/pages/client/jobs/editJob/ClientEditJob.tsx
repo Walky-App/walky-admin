2bd80e1891bc8a4b3615a370d8f1ca219ea674e4
Alex Artemov <artemov.sandro@gmail.com>
2024-04-16 13:40:42 -0400

    cleaned up before pushing

--- src/pages/client/jobs/editJob/ClientEditJob.tsx
+++ src/pages/client/jobs/editJob/ClientEditJob.tsx
@@ -254,7 +254,7 @@ export default function ClientEditJob() {
                         </div>
                       </>
                     )}
-                  />{' '}
+                  />
                 </div>
               </div>
             </div>
