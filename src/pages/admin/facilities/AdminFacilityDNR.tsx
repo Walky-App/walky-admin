66aab9723e3b0732517ef255e65f95e35c377064
Alex Artemov <artemov.sandro@gmail.com>
2024-04-17 01:25:47 -0400

    set up new DNR component with mockup inputs

---
+++ src/pages/admin/facilities/AdminFacilityDNR.tsx
@@ -0,0 +1,230 @@
+/* eslint-disable */
+import { useState, useEffect } from 'react'
+
+import { Controller, type SubmitHandler, useForm, type FieldErrors } from 'react-hook-form'
+
+import { Button } from 'primereact/button'
+import { Calendar } from 'primereact/calendar'
+import { Column } from 'primereact/column'
+import { DataTable } from 'primereact/datatable'
+import { Dropdown } from 'primereact/dropdown'
+import { InputText } from 'primereact/inputtext'
+import { classNames } from 'primereact/utils'
+
+import { useAuth } from '../../../contexts/AuthContext'
+import { RequestService } from '../../../services/RequestService'
+import { useUtils } from '../../../store/useUtils'
+
+interface IHoliday {
+  _id: string
+  holiday_name: string
+  holiday_date: Date | undefined
+  created_by: string
+  updated_by: string
+}
+interface IHolidayFormValues {
+  holiday_name: string
+  holiday_date: Date | undefined
+  created_by: string
+  updated_by: string
+}
+
+export const AdminFacilityDNR = () => {
+  const { showToast } = useUtils()
+  const { user } = useAuth()
+
+  const [formHoliday, setFormHoliday] = useState<IHolidayFormValues>({
+    holiday_name: '',
+    holiday_date: undefined,
+    created_by: user?.email != null ? user.email : '',
+    updated_by: '',
+  })
+
+  const [holidays, setHolidays] = useState<IHoliday[]>([])
+  const [selectedHoliday, setSelectedHoliday] = useState<IHoliday | null>(null)
+
+  useEffect(() => {
+    const getHolidays = async () => {
+      try {
+        const response = await RequestService('holidays', 'GET')
+        setHolidays(response)
+      } catch (error) {
+        console.error('error fetching holidays', error)
+      }
+    }
+    getHolidays()
+  }, [])
+
+  const deleteHoliday = async (holiday: IHoliday) => {
+    try {
+      await RequestService(`holidays/${holiday._id}`, 'DELETE')
+      setHolidays(holidays.filter(existingHoliday => existingHoliday._id !== holiday._id))
+
+      showToast({ severity: 'success', summary: 'Success', detail: 'Holiday deleted successfully' })
+    } catch (error) {
+      console.error('error deleting holiday', error)
+      showToast({ severity: 'error', summary: 'Error', detail: 'Error deleting holiday' })
+    }
+  }
+
+  const defaultValues = {
+    holiday_name: formHoliday?.holiday_name,
+    holiday_date: formHoliday?.holiday_date,
+    created_by: formHoliday?.created_by,
+    updated_by: formHoliday?.updated_by,
+  }
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+    setValue,
+  } = useForm({ defaultValues })
+
+  useEffect(() => {
+    if (formHoliday) {
+      setValue('holiday_name', formHoliday.holiday_name)
+      setValue('holiday_date', formHoliday.holiday_date)
+      setValue('created_by', formHoliday.created_by)
+      setValue('updated_by', formHoliday.updated_by)
+    }
+  }, [formHoliday, setValue])
+
+  function getFormErrorMessage(path: string, errors: FieldErrors) {
+    const pathParts = path.split('.')
+    let error: FieldErrors = errors
+
+    for (const part of pathParts) {
+      if (typeof error !== 'object' || error === null) {
+        return null
+      }
+      error = error[part as keyof typeof error] as FieldErrors
+    }
+
+    if (error?.message) {
+      return error.message ? <p className="mt-2 text-sm text-red-600">{String(error.message)}</p> : null
+    }
+
+    return null
+  }
+
+  const onSubmit: SubmitHandler<IHolidayFormValues> = async data => {
+    try {
+      let response: IHoliday
+      if (selectedHoliday) {
+        const updatedData = { ...data, updated_by: user?.email || '' }
+        response = await RequestService(`holidays/${selectedHoliday._id}`, 'PATCH', updatedData)
+      } else {
+        const newData = { ...data, created_by: user?.email || '' }
+        response = await RequestService('holidays', 'POST', newData)
+      }
+      if (response) {
+        setFormHoliday({
+          holiday_name: '',
+          holiday_date: undefined,
+          created_by: user?.email || '',
+          updated_by: '',
+        })
+        if (selectedHoliday) {
+          setHolidays(holidays.map(holiday => (holiday._id === response._id ? response : holiday)))
+        } else {
+          setHolidays([...holidays, response])
+        }
+        showToast({ severity: 'success', summary: 'Success', detail: 'Holiday created successfully' })
+        reset()
+        setSelectedHoliday(null)
+      }
+    } catch (error) {
+      console.error('Error:', error)
+      showToast({ severity: 'error', summary: 'Error', detail: 'Error creating holiday' })
+    }
+  }
+
+  const updatedByTemplate = (rowData: IHoliday) => {
+    return <span>{rowData.updated_by}</span>
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-12">
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3">
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">DNR list </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Add an employee to the DNR list. This is a blacklist for people who you wouldn't want to apply for jobs of
+              this particular facility. Select a person from the dropdown component and click button to add to the
+              table.
+            </p>
+          </div>
+
+          <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-6 sm:grid-cols-6 md:col-span-2">
+            <div className="sm:col-span-3">
+              <label htmlFor="holiday_name" className="block text-sm font-medium leading-6 text-gray-900">
+                Select Employee
+              </label>
+              <div className="mt-2">
+                <Controller
+                  name="holiday_name"
+                  control={control}
+                  rules={{ required: 'Holiday name is required' }}
+                  render={({ field, fieldState }) => (
+                    <InputText
+                      id={field.name}
+                      value={field.value}
+                      name="holiday_name"
+                      className={classNames({ 'p-invalid': fieldState.error }, 'w-full')}
+                      onChange={e => field.onChange(e.target.value)}
+                    />
+                  )}
+                />
+              </div>
+              {getFormErrorMessage('holiday_name', errors)}
+            </div>
+
+            <div className="flex justify-end sm:col-span-6">
+              <Button type="submit" label="Submit" />
+            </div>
+          </div>
+        </div>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-10 pb-12 md:grid-cols-1">
+          <div>
+            <h1 className="text-xl font-bold leading-7 text-gray-900">DNR Table</h1>
+            <DataTable
+              value={holidays}
+              selectionMode="single"
+              selection={selectedHoliday}
+              onSelectionChange={e => setSelectedHoliday(e.value as IHoliday)}
+              onRowSelect={e => setFormHoliday(e.data)}
+              dataKey="_id"
+              paginator
+              rows={5}
+              metaKeySelection={false}
+              tableStyle={{ minWidth: '50rem' }}>
+              <Column field="holiday_name" header="Avatar" />
+              <Column field="holiday_name" header="Name" />
+              <Column
+                body={(rowData: IHoliday) => (
+                  <span>{rowData.holiday_date ? new Date(rowData.holiday_date).toLocaleDateString() : ''}</span>
+                )}
+                header="Date Added"
+              />
+              <Column field="created_by" header="Added By" />
+              <Column body={updatedByTemplate} header="Reason" />
+              <Column
+                header="Delete"
+                body={rowData => (
+                  <Button
+                    icon="pi pi-trash"
+                    className="p-button-rounded p-button-danger"
+                    onClick={() => deleteHoliday(rowData)}
+                  />
+                )}
+              />
+            </DataTable>
+          </div>
+        </div>
+      </div>
+    </form>
+  )
+}
