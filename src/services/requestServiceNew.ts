99b48b3d3eabc66bc2d6080d252ba16a9a735742
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-04-17 01:54:07 -0400

    fix: simplified and added types to requestService

--- src/services/requestServiceNew.ts
+++ src/services/requestServiceNew.ts
@@ -1,37 +1,29 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { GetTokenInfo } from '../utils/TokenUtils'
 
-type IRequestService = (path: string, method?: string, body?: any, dataType?: string) => Promise<Response>
+type IRequestService = (path: string, method?: string, body?: BodyInit, dataType?: string) => Promise<Response>
 
 export const requestService: IRequestService = async (
   path: string,
   method = 'GET',
-  body: any = null,
+  body: BodyInit | undefined,
   dataType = 'json',
 ): Promise<Response> => {
   const { access_token } = GetTokenInfo()
-
   const url = `${process.env.REACT_APP_PUBLIC_API}/${path}`
 
-  const headersJsonData = {
-    'Content-Type': 'application/json',
+  let headers: HeadersInit = {
     Authorization: `Bearer ${access_token}`,
   }
 
-  const headersBinaryData = {
-    Authorization: `Bearer ${access_token}`,
+  if (dataType === 'json') {
+    headers = { ...headers, 'Content-Type': 'application/json' }
   }
-
-  const headers = dataType === 'json' ? headersJsonData : headersBinaryData
-
-  const bodyData = dataType === 'json' ? JSON.stringify(body) : body
 
   const options: RequestInit = {
     method,
     headers,
-    body: body ? bodyData : undefined,
+    body: body !== undefined && dataType === 'json' ? JSON.stringify(body) : null,
   }
 
-  const response = await fetch(url, options)
-  return response
+  return fetch(url, options)
 }
