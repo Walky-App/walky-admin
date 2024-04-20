6c7f930a4fad1ca3a7973cd0a5d41fd52ed0eec8
Jorge Poveda <jpoveda731@gmail.com>
2024-04-20 13:56:12 -0400

    fix: video sticky

--- src/pages/learn/units/UnitDetail.tsx
+++ src/pages/learn/units/UnitDetail.tsx
@@ -53,7 +53,11 @@ export const UnitDetail = () => {
       <div className="mt-4 grid grid-cols-4 gap-6 md:grid-cols-3">
         {/*left content*/}
         <div className="order-1 col-span-4 rounded-2xl border border-zinc-100 bg-white md:col-span-2">
-          {unit?.type === 'video' ? <VideoPlayer url={unit.url_video} captions={unit.url_captions} /> : null}
+          {unit?.type === 'video' ? (
+            <div className="sticky top-[64px] z-50">
+              <VideoPlayer url={unit.url_video} captions={unit.url_captions} title={unit.title} />
+            </div>
+          ) : null}
           <div className="flex h-auto flex-col ">{unit ? <UnitDetailsCard unit={unit} /> : null}</div>
         </div>
 
