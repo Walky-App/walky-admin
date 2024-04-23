a4ef0671d56efe29b19c303f4768a390b6e18b7d
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-04-23 13:37:13 -0400

    fix: confirmPopup not dismissed when Image Dialog closed

--- src/pages/admin/facilities/AdminFacilityImages.tsx
+++ src/pages/admin/facilities/AdminFacilityImages.tsx
@@ -152,7 +152,6 @@ export const AdminFacilityImages = () => {
 
   return (
     <div>
-      <ConfirmPopup />
       <SubHeader data={facility} links={adminFacilitiesLinks} />
       <input
         ref={filesInputRef}
@@ -224,6 +223,7 @@ export const AdminFacilityImages = () => {
                 leaveFrom="opacity-100 translate-y-0 sm:scale-100"
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95">
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+                  <ConfirmPopup />
                   <div className="absolute right-0 top-0 hidden pb-4 pl-4 pr-1 pt-1 sm:block">
                     <button
                       type="button"
