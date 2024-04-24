f431b70d2f040f1f530f5be5aa8d61b7779019bc
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-04-24 16:50:07 -0400

    worked on time_in and time_out cell edit in AdminUserTimeSheets

--- src/pages/admin/users/components/AdminUserTimeSheets.tsx
+++ src/pages/admin/users/components/AdminUserTimeSheets.tsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import React, { useCallback, useEffect, useMemo, useState } from 'react'
 
 import { Calendar } from 'primereact/calendar'
-import { Column, type ColumnEditorOptions } from 'primereact/column'
+import { Column, type ColumnEvent, type ColumnEditorOptions } from 'primereact/column'
 import { DataTable, type DataTableExpandedRows, type DataTableValueArray } from 'primereact/datatable'
 
 import { type ITimeSheet } from '../../../../interfaces/timesheet'
+import { requestService } from '../../../../services/requestServiceNew'
+import { useUtils } from '../../../../store/useUtils'
+import { cn } from '../../../../utils/cn'
 import { GetTokenInfo } from '../../../../utils/tokenUtil'
 import { useAdminUserContext } from '../AdminUserPage'
 
@@ -12,6 +15,16 @@ interface IPunchDetails {
   in_time: string
   out_time: string
   total_time: string
+}
+
+interface IPunchPair {
+  _id: string
+  time_stamp: string
+  day: string
+  in_time: string
+  out_time: string
+  total_time: string
+  timesheet_id: string
 }
 interface IProcessedTimeSheet {
   time_stamp: string
@@ -30,6 +43,8 @@ export const AdminUserTimeSheets = () => {
   const [processedTimeSheets, setProcessedTimeSheets] = useState<IProcessedTimeSheet[]>([])
   const [expandedRows, setExpandedRows] = useState<DataTableExpandedRows | DataTableValueArray | undefined>(undefined)
 
+  const { showToast } = useUtils()
+
   const { selectedUserId } = useAdminUserContext()
 
   const formatDate = (dateString: string) => {
@@ -41,125 +56,131 @@ export const AdminUserTimeSheets = () => {
     return date.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: true })
   }
 
-  useEffect(() => {
-    const fetchTimeSheets = async () => {
-      if (!selectedUserId) {
-        console.error('selectedUserId is undefined')
-        return
-      }
-      const { access_token } = GetTokenInfo()
-      const url = `${process.env.REACT_APP_PUBLIC_API}/timesheets/employee/${selectedUserId}`
-
-      const options: RequestInit = {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${access_token}`,
-        },
-      }
-
-      try {
-        const response = await fetch(url, options)
-
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`)
-        }
-
-        if (response.status === 204) {
-          console.error('No timesheets found')
-        } else {
-          const data: ITimeSheet[] = await response.json()
-
-          const processedData: IProcessedTimeSheet[] = data.map(timeSheet => {
-            const sortedPunches = [...timeSheet.punches].sort(
-              (a, b) => new Date(a.time_stamp).getTime() - new Date(b.time_stamp).getTime(),
-            )
-
-            let totalWorkedTime = 0
-            let punchIn = null
-            let inTime = ''
-            let outTime = ''
-            for (const punch of sortedPunches) {
-              if (punch.punch_in) {
-                punchIn = punch
-                if (!inTime) {
-                  inTime = formatTime(punch.time_stamp)
-                }
-              } else if (punchIn) {
-                const totalTime = Date.parse(punch.time_stamp) - Date.parse(punchIn.time_stamp)
-                totalWorkedTime += totalTime
-                outTime = formatTime(punch.time_stamp)
-                punchIn = null
-              }
-            }
-
-            if (!outTime && punchIn) {
-              const totalTime = Date.now() - Date.parse(punchIn.time_stamp)
-              totalWorkedTime += totalTime
-            }
-
-            const totalWorkedHours = totalWorkedTime / 1000 / 60 / 60
-
-            const day = formatDate(sortedPunches[0].time_stamp)
-
-            return {
-              time_stamp: sortedPunches[0].time_stamp,
-              day,
-              in_time: inTime,
-              out_time: outTime,
-              total_time: totalWorkedHours.toFixed(2), // total worked hours
-              details: timeSheet.job_id, // generate details
-              worked_time: totalWorkedHours.toFixed(2), // total worked hours
-              scheduled_time: '', // calculate scheduled
-              difference: '', // calculate difference
-              punchesWithDetails: (() => {
-                const punchPairs = []
-                let punchIn = null
-
-                for (const punch of sortedPunches) {
-                  if (punch.punch_in) {
-                    punchIn = punch
-                  } else if (punchIn) {
-                    const totalTime = Date.parse(punch.time_stamp) - Date.parse(punchIn.time_stamp)
-                    punchPairs.push({
-                      in_time: formatTime(punchIn.time_stamp),
-                      out_time: formatTime(punch.time_stamp),
-                      total_time: (totalTime / 1000 / 60 / 60).toFixed(2), // total time in hours
-                    })
-                    punchIn = null
-                  }
-                }
-
-                // If there's a punch-in without a corresponding punch-out, calculate the total time until now
-                if (punchIn) {
-                  const totalTime = Date.now() - Date.parse(punchIn.time_stamp)
-                  punchPairs.push({
-                    in_time: formatTime(punchIn.time_stamp),
-                    out_time: '',
-                    total_time: (totalTime / 1000 / 60 / 60).toFixed(2), // total time in hours
-                  })
-                }
-
-                return punchPairs
-              })(),
-            } as IProcessedTimeSheet
-          })
-          setProcessedTimeSheets(processedData)
-        }
-      } catch (error) {
-        console.error('Failed to fetch timesheet:', error)
-      }
+  const fetchTimesheets = useCallback(async () => {
+    if (!selectedUserId) {
+      console.error('selectedUserId is undefined')
+      return
     }
-    fetchTimeSheets()
+    const { access_token } = GetTokenInfo()
+    const url = `${process.env.REACT_APP_PUBLIC_API}/timesheets/employee/${selectedUserId}`
+
+    const options: RequestInit = {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${access_token}`,
+      },
+    }
+
+    try {
+      const response = await fetch(url, options)
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`)
+      }
+
+      if (response.status === 204) {
+        console.error('No timesheets found')
+      } else {
+        const data: ITimeSheet[] = await response.json()
+
+        const processedData: IProcessedTimeSheet[] = data.map(timeSheet => {
+          const sortedPunches = [...timeSheet.punches].sort(
+            (a, b) => new Date(a.time_stamp).getTime() - new Date(b.time_stamp).getTime(),
+          )
+
+          let totalWorkedTime = 0
+          let punchIn = null
+          let inTime = ''
+          let outTime = ''
+          for (const punch of sortedPunches) {
+            if (punch.punch_in) {
+              punchIn = punch
+              if (!inTime) {
+                inTime = formatTime(punch.time_stamp)
+              }
+              outTime = ''
+            } else if (punchIn) {
+              const totalTime = Date.parse(punch.time_stamp) - Date.parse(punchIn.time_stamp)
+              totalWorkedTime += totalTime
+              outTime = formatTime(punch.time_stamp)
+              punchIn = null
+            }
+          }
+
+          if (!outTime && punchIn) {
+            const totalTime = Date.now() - Date.parse(punchIn.time_stamp)
+            totalWorkedTime += totalTime
+          }
+
+          const totalWorkedHours = totalWorkedTime / 1000 / 60 / 60
+
+          const day = formatDate(sortedPunches[0].time_stamp)
+
+          return {
+            time_stamp: sortedPunches[0].time_stamp,
+            day,
+            in_time: inTime,
+            out_time: outTime,
+            total_time: totalWorkedHours.toFixed(2), // total worked hours
+            details: timeSheet.job_id, // generate details
+            worked_time: totalWorkedHours.toFixed(2), // total worked hours
+            scheduled_time: '', // calculate scheduled
+            difference: '', // calculate difference
+            punchesWithDetails: (() => {
+              const punchPairs: IPunchPair[] = [] as IPunchPair[]
+              let punchIn = null
+
+              for (const punch of sortedPunches) {
+                if (punch.punch_in) {
+                  punchIn = punch
+                } else if (punchIn) {
+                  const totalTime = Date.parse(punch.time_stamp) - Date.parse(punchIn.time_stamp)
+                  punchPairs.push({
+                    _id: punch._id,
+                    time_stamp: punch.time_stamp,
+                    day: formatDate(punch.time_stamp),
+                    in_time: formatTime(punchIn.time_stamp),
+                    out_time: formatTime(punch.time_stamp),
+                    total_time: (totalTime / 1000 / 60 / 60).toFixed(2), // total time in hours
+                    timesheet_id: timeSheet._id,
+                  })
+                  punchIn = null
+                }
+              }
+
+              // If there's a punch-in without a corresponding punch-out, calculate the total time until now
+              if (punchIn) {
+                const totalTime = Date.now() - Date.parse(punchIn.time_stamp)
+                punchPairs.push({
+                  _id: punchIn._id,
+                  time_stamp: punchIn.time_stamp,
+                  day: formatDate(punchIn.time_stamp),
+                  in_time: formatTime(punchIn.time_stamp),
+                  out_time: '',
+                  total_time: (totalTime / 1000 / 60 / 60).toFixed(2), // total time in hours
+                  timesheet_id: timeSheet._id,
+                })
+              }
+
+              return punchPairs
+            })(),
+          } as IProcessedTimeSheet
+        })
+        setProcessedTimeSheets(processedData)
+      }
+    } catch (error) {
+      console.error('Failed to fetch timesheet:', error)
+    }
   }, [selectedUserId])
+
+  useEffect(() => {
+    fetchTimesheets()
+  }, [fetchTimesheets, selectedUserId])
 
   const sortedTimeSheets = useMemo(() => {
     return [...processedTimeSheets].sort((a, b) => new Date(b.time_stamp).getTime() - new Date(a.time_stamp).getTime())
   }, [processedTimeSheets])
-
-  const timeEditor = (options: ColumnEditorOptions) => {
-    return <Calendar value={options.value} onChange={e => options.editorCallback!(e.target.value)} timeOnly />
-  }
 
   interface IColumnMeta<T> {
     field: keyof T
@@ -168,23 +189,65 @@ export const AdminUserTimeSheets = () => {
     editor?: (options: ColumnEditorOptions) => React.ReactNode
   }
 
-  const cols: IColumnMeta<IProcessedTimeSheet>[] = [
-    { field: 'day', header: 'Day', sortable: true },
-    { field: 'in_time', header: 'In', sortable: false, editor: timeEditor },
-    { field: 'out_time', header: 'Out', sortable: false, editor: timeEditor },
-    { field: 'total_time', header: 'Total', sortable: false },
-    { field: 'details', header: 'Job Details', sortable: false },
-    { field: 'worked_time', header: 'Worked', sortable: false },
-    { field: 'scheduled_time', header: 'Scheduled', sortable: false },
-    { field: 'difference', header: 'Difference', sortable: false },
-  ]
-
   // const getEditor = (col: IColumnMeta<IProcessedTimeSheet>) => {
   //   if (col.field === 'in_time' || col.field === 'out_time') {
   //     return (options: ColumnEditorOptions) => (col.editor ? col.editor(options) : undefined)
   //   }
   //   return undefined
   // }
+
+  const timeEditor = (options: ColumnEditorOptions) => {
+    const { time_stamp } = options.rowData as IPunchPair
+    let punchPairDate: Date | null = null
+
+    if (time_stamp && !isNaN(Date.parse(time_stamp))) {
+      punchPairDate = new Date(time_stamp)
+    }
+
+    return <Calendar value={punchPairDate} hourFormat="12" onChange={e => options.editorCallback!(e.value)} timeOnly />
+  }
+
+  const onCellEditComplete = async (e: ColumnEvent) => {
+    const { rowData, newValue, field } = e as { rowData: IPunchPair; newValue: string; field: string }
+
+    if (field === 'in_time' || field === 'out_time') {
+      const newTimeStamp = new Date(newValue).toISOString()
+      const punchId = rowData._id
+      const timeSheetId = rowData.timesheet_id
+
+      const body =
+        field === 'in_time'
+          ? { punch_in_id: punchId, new_time_stamp_in: newTimeStamp }
+          : { punch_out_id: punchId, new_time_stamp_out: newTimeStamp }
+
+      const response = await requestService({
+        path: `timesheets/${timeSheetId}/in-out-punches`,
+        method: 'PATCH',
+        body: JSON.stringify(body),
+      })
+
+      if (!response.ok) {
+        const message = await response.text()
+        showToast({ severity: 'error', summary: 'Error', detail: message })
+        throw new Error(message)
+      }
+
+      fetchTimesheets()
+    }
+  }
+
+  const cols: IColumnMeta<IProcessedTimeSheet>[] = [
+    { field: 'day', header: 'Day', sortable: true },
+    { field: 'in_time', header: 'In', sortable: false },
+    // { field: 'in_time', header: 'In', sortable: false, editor: timeEditor },
+    { field: 'out_time', header: 'Out', sortable: false },
+    // { field: 'out_time', header: 'Out', sortable: false, editor: timeEditor },
+    { field: 'total_time', header: 'Total', sortable: false },
+    { field: 'details', header: 'Job Details', sortable: false },
+    { field: 'worked_time', header: 'Worked', sortable: false },
+    { field: 'scheduled_time', header: 'Scheduled', sortable: false },
+    { field: 'difference', header: 'Difference', sortable: false },
+  ]
 
   const getCellValue = (col: IColumnMeta<IProcessedTimeSheet>, rowData: IProcessedTimeSheet) => {
     const field = col.field as keyof IProcessedTimeSheet
@@ -208,13 +271,28 @@ export const AdminUserTimeSheets = () => {
   }
 
   const rowExpansionTemplate = (data: IProcessedTimeSheet) => {
+    const commonColumnStyle = 'w-1/12 md:h-20'
     return (
       <div className="p-4">
         <h5 className="text-sm font-semibold">Punch Details</h5>
-        <DataTable value={data.punchesWithDetails} sortField="in_time" sortOrder={-1}>
-          <Column field="in_time" header="In" />
-          <Column field="out_time" header="Out" body={(rowData: IPunchDetails) => rowData.out_time || 'Clocked In'} />
-          <Column field="total_time" header="Total" />
+        <DataTable value={data.punchesWithDetails} sortField="in_time" sortOrder={-1} editMode="cell">
+          <Column className={cn([commonColumnStyle])} field="day" header="Day" />
+          <Column
+            className={cn([commonColumnStyle])}
+            field="in_time"
+            header="In (✎)"
+            editor={options => timeEditor(options)}
+            onCellEditComplete={onCellEditComplete}
+          />
+          <Column
+            className={cn([commonColumnStyle])}
+            field="out_time"
+            header="Out (✎)"
+            body={(rowData: IPunchDetails) => rowData.out_time || 'Clocked In'}
+            editor={options => timeEditor(options)}
+            onCellEditComplete={onCellEditComplete}
+          />
+          <Column className={cn([commonColumnStyle, 'w-4/12'])} field="total_time" header="Total" />
         </DataTable>
       </div>
     )
