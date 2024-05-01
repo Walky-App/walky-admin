16292808658a3f41959974d0da1e985d6f9c3e47
jonathans199 <jonathans199@gmail.com>
2024-05-01 12:25:52 -0400

    updated sign up form to use new request service prime react and updated api error messages

--- src/pages/auth/SignupForm.tsx
+++ src/pages/auth/SignupForm.tsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from 'react'
 
+import { Controller, type SubmitHandler, useForm } from 'react-hook-form'
 import { useNavigate, useParams } from 'react-router-dom'
 
 import { InputMask } from 'primereact/inputmask'
@@ -7,9 +8,8 @@ import { InputText } from 'primereact/inputtext'
 import { Password } from 'primereact/password'
 import { Toast } from 'primereact/toast'
 
-import { AddressAutoComplete, type IAddressAutoComplete } from '../../components/shared/forms/AddressAutoComplete'
 import { useAuth } from '../../contexts/AuthContext'
-import { type ILoginData } from '../../interfaces/loginData'
+import { type ISignupData, type ILoginData } from '../../interfaces/loginData'
 import { type ITokenInfo } from '../../interfaces/services'
 import { requestService } from '../../services/requestServiceNew'
 import { useUtils } from '../../store/useUtils'
@@ -21,19 +21,10 @@ const employee_role = process.env.REACT_APP_EMPLOYEE_ROLE as string
 const sales_role = process.env.REACT_APP_SALES_ROLE as string
 
 export const Signup = () => {
-  const [error, setError] = useState()
+  const { showToast } = useUtils()
   const [loading, setLoading] = useState(false)
-  const [form, setForm] = useState({
-    _id: '',
-    access_token: '',
-    password: '',
-    password_confirmed: '',
-    phone_number: '',
-    address: '',
-  })
   const { setUser } = useAuth()
   const { setAvatarImageUrl } = useUtils()
-  const [moreAddressDetails, setMoreAddressDetails] = useState<IAddressAutoComplete>()
   const toast = useRef<Toast>(null)
 
   const show = (message: string) => {
@@ -43,64 +34,79 @@ export const Signup = () => {
   const { email, role } = useParams()
   const navigate = useNavigate()
 
-  const handleSignup = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+  const defaultValues: ISignupData = {
+    first_name: '',
+    last_name: '',
+    password: '',
+    password_confirmed: '',
+    email: email as string,
+    role: role as string,
+    phone_number: '',
+    otp: '',
+  }
+
+  const { control, handleSubmit, reset } = useForm({ defaultValues })
+
+  const onSubmit: SubmitHandler<ISignupData> = async data => {
     await setLoading(true)
 
-    const target = e.target as HTMLFormElement
-
-    const formData = {
-      email: email,
-      phone_number: target.phone.value,
-      password: target.password.value,
-      password_confirmed: target.password_confirmed.value,
-      first_name: target.firstName.value,
-      last_name: target.lastName.value,
-      role: role,
-    }
-
     try {
-      const response = await requestService({ path: 'auth', method: 'POST', body: JSON.stringify(formData) })
-      const data = await response.json()
+      const payload = {
+        first_name: data.first_name,
+        last_name: data.last_name,
+        password: data.password,
+        password_confirmed: data.password_confirmed,
+        email: email as string,
+        role: role as string,
+        phone_number: data.phone_number,
+        otp: data.otp,
+      }
 
-      const { access_token, user }: ILoginData = data
+      const response = await requestService({ path: 'auth', method: 'POST', body: JSON.stringify(payload) })
+      const jsonResponse = await response.json()
 
-      if (access_token) {
-        const ls_info: ITokenInfo = {
-          first_name: user.first_name,
-          _id: user._id,
-          role: user.role,
-          access_token: access_token,
-          avatar: user.avatar,
-        }
-
-        SetToken(ls_info)
-        setUser({ ...user, access_token: access_token })
-        setAvatarImageUrl(user.avatar as string)
-        setLoading(false)
-
-        switch (user.role) {
-          case client_role:
-            navigate('/client/dashboard')
-            break
-          case admin_role:
-            navigate('/admin/dashboard')
-            break
-          case employee_role:
-            navigate('/employee/dashboard')
-            break
-          case sales_role:
-            navigate('/sales/dashboard')
-            break
-          default:
-            navigate('/login')
+      if (response.status === 201) {
+        const { access_token, user }: ILoginData = jsonResponse
+        if (access_token) {
+          const ls_info: ITokenInfo = {
+            first_name: user.first_name,
+            _id: user._id,
+            role: user.role,
+            access_token: access_token,
+            avatar: user.avatar,
+          }
+          SetToken(ls_info)
+          setUser({ ...user, access_token: access_token })
+          setAvatarImageUrl(user.avatar as string)
+          setLoading(false)
+          switch (user.role) {
+            case client_role:
+              navigate('/client/dashboard')
+              break
+            case admin_role:
+              navigate('/admin/dashboard')
+              break
+            case employee_role:
+              navigate('/employee/dashboard')
+              break
+            case sales_role:
+              navigate('/sales/dashboard')
+              break
+            default:
+              navigate('/login')
+          }
+          showToast({
+            severity: 'success',
+            summary: 'Success',
+            detail: 'Employee added to DNR list successfully',
+          })
+          reset()
         }
       } else {
-        setError(data.message)
         setLoading(false)
       }
     } catch (error) {
-      console.error('Error:', error)
+      showToast({ severity: 'error', summary: 'Error', detail: 'An error occurred. Please try again.' })
       show('An error occurred. Please try again.')
       setLoading(false)
     }
@@ -108,35 +114,59 @@ export const Signup = () => {
 
   return (
     <div className="flex h-screen items-center justify-center">
-      <form onSubmit={handleSignup} className="mx-auto max-w-md">
+      <form onSubmit={handleSubmit(onSubmit)} className="mx-auto max-w-md">
         <div className="flex justify-center">
           <img src="/assets/logos/logo-horizontal-cropped.png" alt="hemp temps logo" width={400} />
         </div>
         <div className="mb-5 grid grid-cols-1 gap-5 sm:grid-cols-2">
-          <InputText
-            required
-            type="text"
+          <Controller
             name="first_name"
-            className="w-full rounded-lg border-zinc-200 p-4 shadow-sm  focus:border-green-500 focus:ring-green-500"
-            placeholder="*First Name"
+            control={control}
+            rules={{ required: 'First Name is required' }}
+            render={({ field }) => (
+              <InputText
+                required
+                type="text"
+                name="first_name"
+                onChange={e => field.onChange(e.target.value)}
+                className="w-full rounded-lg border-zinc-200 p-4 shadow-sm  focus:border-green-500 focus:ring-green-500"
+                placeholder="*First Name"
+              />
+            )}
           />
-
-          <InputText
-            required
-            type="text"
+          <Controller
             name="last_name"
-            className="w-full rounded-lg border-zinc-200 p-4 shadow-sm  focus:border-green-500 focus:ring-green-500"
-            placeholder="*Last Name"
+            control={control}
+            rules={{ required: 'Last Name is required' }}
+            render={({ field }) => (
+              <InputText
+                required
+                type="text"
+                name="last_name"
+                onChange={e => field.onChange(e.target.value)}
+                className="w-full rounded-lg border-zinc-200 p-4 shadow-sm  focus:border-green-500 focus:ring-green-500"
+                placeholder="*Last Name"
+              />
+            )}
           />
         </div>
-
         <div className="relative mb-5">
-          <InputText
-            required
-            type="email"
+          <Controller
             name="email"
-            className="w-full rounded-lg border-zinc-200 p-4 shadow-sm  focus:border-green-500 focus:ring-green-500"
-            placeholder="*Email"
+            control={control}
+            rules={{ required: 'Email is required' }}
+            render={({ field }) => (
+              <InputText
+                required
+                disabled
+                defaultValue={email}
+                type="email"
+                name="email"
+                onChange={e => field.onChange(e.target.value)}
+                className="w-full rounded-lg border-zinc-200 p-4 shadow-sm  focus:border-green-500 focus:ring-green-500"
+                placeholder="*Email"
+              />
+            )}
           />
           <span className="absolute inset-y-0 end-0 grid place-content-center px-4">
             <svg
@@ -155,67 +185,88 @@ export const Signup = () => {
           </span>
         </div>
         <div className="relative my-5">
-          {/* <InputText
-            required
-            type="tel"
-            name="phone"
-            className="w-full rounded-lg border-zinc-200 p-4 shadow-sm  focus:border-green-500 focus:ring-green-500"
-            placeholder="*Phone Number"
-          /> */}
-
-          <InputMask
-            value={form.phone_number}
-            onChange={e => setForm({ ...form, phone_number: e.target.value || '' })}
-            mask="(999) 999-9999"
-            placeholder="(561) 999-9999"
-            className="w-full"
-          />
-        </div>
-        <div className="p-fluid mb-5">
-          <AddressAutoComplete
-            setMoreAddressDetails={setMoreAddressDetails}
-            className="rounded-lg border-zinc-200  focus:border-green-500 focus:ring-green-500"
-            currentAddress={'*Home Address'}
+          <Controller
+            name="phone_number"
+            control={control}
+            rules={{ required: 'Phone Number is required' }}
+            render={({ field }) => (
+              <InputMask
+                required
+                mask="(999) 999-9999"
+                onChange={e => field.onChange(e.target.value)}
+                placeholder="(561) 999-9999"
+                className="w-full"
+              />
+            )}
           />
         </div>
         <div>
-          <Password
-            inputId="password"
-            placeholder="*Password"
-            value={form.password}
-            onChange={e => setForm({ ...form, password: e.target.value })}
-            toggleMask
-            pt={{
-              panel: { className: 'hidden' },
-              input: {
-                className:
-                  'w-full rounded-lg border-zinc-200 p-4 shadow-sm focus:border-green-500 focus:ring-green-500',
-              },
-            }}
-            className="w-full"
+          <Controller
+            name="password"
+            control={control}
+            rules={{ required: 'Password is required' }}
+            render={({ field }) => (
+              <Password
+                required
+                inputId="password"
+                placeholder="*Password"
+                toggleMask
+                onChange={e => field.onChange(e.target.value)}
+                pt={{
+                  panel: { className: 'hidden' },
+                  input: {
+                    className:
+                      'w-full rounded-lg border-zinc-200 p-4 shadow-sm focus:border-green-500 focus:ring-green-500',
+                  },
+                }}
+                className="w-full"
+              />
+            )}
           />
-          <Password
-            inputId="password_confirmed"
-            placeholder="*Password confirmed"
-            value={form.password_confirmed}
-            onChange={e => setForm({ ...form, password_confirmed: e.target.value })}
-            toggleMask
-            pt={{
-              panel: { className: 'hidden' },
-              input: {
-                className:
-                  'w-full rounded-lg border-zinc-200 p-4 shadow-sm focus:border-green-500 focus:ring-green-500',
-              },
-            }}
-            className="my-5 w-full"
+          <Controller
+            name="password_confirmed"
+            control={control}
+            rules={{ required: 'Password Verify is required' }}
+            render={({ field }) => (
+              <Password
+                required
+                inputId="password_confirmed"
+                placeholder="*Password confirmed"
+                toggleMask
+                onChange={e => field.onChange(e.target.value)}
+                pt={{
+                  panel: { className: 'hidden' },
+                  input: {
+                    className:
+                      'w-full rounded-lg border-zinc-200 p-4 shadow-sm focus:border-green-500 focus:ring-green-500',
+                  },
+                }}
+                className="my-5 w-full"
+              />
+            )}
           />
-          <Toast ref={toast} position="bottom-right" />
+          {/* <div className="relative my-5">
+            <label htmlFor="employee" className="block text-sm font-medium leading-6 text-gray-900">
+              One Time Passcode
+            </label>
+            <Controller
+              name="otp"
+              control={control}
+              rules={{ required: 'One time passcode is required' }}
+              render={({ field }) => (
+                <InputMask
+                  required
+                  mask="999-999"
+                  onChange={e => field.onChange(e.target.value)}
+                  placeholder="123-456"
+                  className="w-full"
+                />
+              )}
+            />
 
-          {error ? (
-            <div className="mt-3 flex items-center justify-center">
-              <p className="text-sm text-red-500">{error}</p>
-            </div>
-          ) : null}
+          </div> */}
+
+          <Toast ref={toast} position="bottom-right" />
         </div>
         <div className="flex items-center justify-center">
           <button
