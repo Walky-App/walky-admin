4f41b7770025b546d0a47d758edc4e4ce636ecc0
jonathans199 <jonathans199@gmail.com>
2024-05-13 19:20:14 -0400

    started tailwindui sidebar-sub-menu - wip

--- src/components/layout/SideBarComponent.tsx
+++ src/components/layout/SideBarComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/strict-boolean-expressions */
 import { type Dispatch, type SetStateAction, useState } from 'react'
 
 import { NavLink, Link } from 'react-router-dom'
@@ -5,6 +6,17 @@ import { NavLink, Link } from 'react-router-dom'
 import cn from 'classnames'
 import { Button } from 'primereact/button'
 import { Dialog } from 'primereact/dialog'
+
+import { Disclosure } from '@headlessui/react'
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import {
+  CalendarIcon,
+  ChartPieIcon,
+  DocumentDuplicateIcon,
+  FolderIcon,
+  HomeIcon,
+  UsersIcon,
+} from '@heroicons/react/24/outline'
 
 import { useAuth } from '../../contexts/AuthContext'
 import { getCurrentUserRole } from '../../utils/UserRole'
@@ -15,6 +27,34 @@ interface SidebarComponentProps {
   sidebarOpen: boolean
   setSidebarOpen: Dispatch<SetStateAction<boolean>>
 }
+
+const navigation = [
+  { name: 'Dashboard', href: '#', icon: HomeIcon, current: true },
+  {
+    name: 'Teams',
+    icon: UsersIcon,
+    current: false,
+    children: [
+      { name: 'Engineering', href: '#' },
+      { name: 'Human Resources', href: '#' },
+      { name: 'Customer Success', href: '#' },
+    ],
+  },
+  {
+    name: 'Projects',
+    icon: FolderIcon,
+    current: false,
+    children: [
+      { name: 'GraphQL API', href: '#' },
+      { name: 'iOS App', href: '#' },
+      { name: 'Android App', href: '#' },
+      { name: 'New Customer Portal', href: '#' },
+    ],
+  },
+  { name: 'Calendar', href: '#', icon: CalendarIcon, current: false },
+  { name: 'Documents', href: '#', icon: DocumentDuplicateIcon, current: false },
+  { name: 'Reports', href: '#', icon: ChartPieIcon, current: false },
+]
 
 export const SidebarComponent = ({ sidebarOpen, setSidebarOpen }: SidebarComponentProps) => {
   const [visible, setVisible] = useState(false)
@@ -29,7 +69,6 @@ export const SidebarComponent = ({ sidebarOpen, setSidebarOpen }: SidebarCompone
         'ring-1 ring-white/10': sidebarOpen,
       })}>
       <div className="mt-4 flex shrink-0 items-center justify-center">
-        {/* Logo */}
         <Link to={user ? `/${role}/dashboard` : '/'}>{LogosPack('sidebar')}</Link>
       </div>
       <nav className="flex flex-1 flex-col">
@@ -92,6 +131,68 @@ export const SidebarComponent = ({ sidebarOpen, setSidebarOpen }: SidebarCompone
                     }
                   })
                 : null}
+            </ul>
+          </li>
+        </ul>
+      </nav>
+
+      <nav className="flex flex-1 flex-col">
+        <ul className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul className="-mx-2 space-y-1">
+              {links.map(item => (
+                <li key={item.name}>
+                  {item.children ? (
+                    <a
+                      href={item.href}
+                      className={cn(
+                        // item.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                        'group flex gap-x-3 rounded-md p-2 text-sm font-semibold leading-6 text-gray-700',
+                      )}>
+                      {item.icon}
+                      {item.name}
+                    </a>
+                  ) : (
+                    <Disclosure as="div">
+                      {({ open }) => (
+                        <>
+                          <Disclosure.Button
+                            className={cn(
+                              // item.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                              'flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm font-semibold leading-6 text-gray-700',
+                            )}>
+                            {item.icon}
+                            {item.name}
+                            <ChevronRightIcon
+                              className={cn(
+                                open ? 'rotate-90 text-gray-500' : 'text-gray-400',
+                                'ml-auto h-5 w-5 shrink-0',
+                              )}
+                              aria-hidden="true"
+                            />
+                          </Disclosure.Button>
+                          <Disclosure.Panel as="ul" className="mt-1 px-2">
+                            {item?.children?.map(subItem => (
+                              <li key={subItem.name}>
+                                {/* 44px */}
+                                <Disclosure.Button
+                                  as="a"
+                                  href={subItem.href}
+                                  className={cn(
+                                    // subItem.current ? 'bg-gray-50' : 'hover:bg-gray-50',
+                                    'block rounded-md py-2 pl-9 pr-2 text-sm leading-6 text-gray-700',
+                                  )}>
+                                  {subItem.name}
+                                </Disclosure.Button>
+                              </li>
+                            ))}
+                          </Disclosure.Panel>
+                        </>
+                      )}
+                    </Disclosure>
+                  )}
+                </li>
+              ))}
             </ul>
           </li>
         </ul>
