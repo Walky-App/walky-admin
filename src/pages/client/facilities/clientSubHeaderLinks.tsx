a17ad6832e7f08529c227a7d3c4f1cf1d4fcc1d4
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-06-27 13:38:51 -0400

    fix: clientSubHeaderLinks contain duplicate `id`s error in browser console

--- src/pages/client/facilities/clientSubHeaderLinks.tsx
+++ src/pages/client/facilities/clientSubHeaderLinks.tsx
@@ -17,6 +17,6 @@ export const clientFacilitiesLink: SubHeaderLink[] = [
   { id: 3, name: 'Jobs', href: '/jobs', icon: <BriefcaseIcon /> },
   { id: 4, name: 'Notes', href: '/internal_notes', icon: <PaperClipIcon /> },
   { id: 5, name: 'Licenses', href: '/licenses', icon: <IdentificationIcon /> },
-  { id: 5, name: 'Activity', href: '/activity', icon: <IdentificationIcon /> },
-  { id: 6, name: 'Docs', href: '/documents', icon: <DocumentPlusIcon />, disabled: true },
+  { id: 6, name: 'Activity', href: '/activity', icon: <IdentificationIcon /> },
+  { id: 7, name: 'Docs', href: '/documents', icon: <DocumentPlusIcon />, disabled: true },
 ]
