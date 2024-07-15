894196354400d45336968cbe6374f0387c9172da
Alex Artemov <146786356+alexartemov7@users.noreply.github.com>
2024-07-15 08:26:30 -0400

    service order rendering correct values after model change

--- src/interfaces/serviceOrder.ts
+++ src/interfaces/serviceOrder.ts
@@ -1,23 +1,23 @@
 import { type ICompany } from './company'
 import { type IFacility } from './facility'
 import { type IJob } from './job'
-
-interface ICostHistory {
-  hours: number
-  pay_rate: number
-  overtime: number
-  shifts: number
-  start_time: string
-  end_time: string
-  cost: number
-  _id: string
-}
+import { type StatesSettingsDocument } from './setting'
 
 interface IDetails {
-  admin_cost: number
-  our_fee: number
-  processing_fee: number
-  cost_history: ICostHistory[]
+  temp_pay_rate: number
+  number_of_vacancies: number
+  number_of_selected_working_days: number
+  number_of_holidays: number
+  supervisor_fees: number
+  total_overtime_fees: number
+  total_hours_per_day: number
+  total_of_all_temps_hours: number
+  total_base_amount: number
+  admin_costs_total: number
+  our_fee_total: number
+  processing_fee_total: number
+  estimated_total_per_hour: number
+  state_settings_history: StatesSettingsDocument
   total_cost: number
 }
 
