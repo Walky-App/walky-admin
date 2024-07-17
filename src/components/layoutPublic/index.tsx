585f050ccf4e3a3f1c0159f3c5472949cc835939
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-07-17 11:21:28 -0400

    chore: bring back Header component in SignupForm

--- src/components/layoutPublic/index.tsx
+++ src/components/layoutPublic/index.tsx
@@ -1,21 +1,26 @@
 import { Outlet } from 'react-router-dom'
 
+import { Header } from './Header'
+
 export const LayoutPublic = () => {
   return (
-    <div className="container mx-auto max-w-6xl">
-      <Outlet />
-      <footer className="bg-white py-6" aria-labelledby="footer-heading">
-        <div className="pt-8 text-center md:items-center md:justify-center">
-          <p className="mt-24 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-            Copyright © {new Date().getFullYear()} Hemp Temps
-          </p>
-          <p className="mt-24 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-            <a href="https://hemptemps.com/terms-and-conditions/" target="_blank" className="underline">
-              Terms and Conditions
-            </a>
-          </p>
-        </div>
-      </footer>
-    </div>
+    <>
+      <Header />
+      <div className="container mx-auto max-w-6xl">
+        <Outlet />
+        <footer className="bg-white py-6" aria-labelledby="footer-heading">
+          <div className="pt-8 text-center md:items-center md:justify-center">
+            <p className="mt-24 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
+              Copyright © {new Date().getFullYear()} Hemp Temps
+            </p>
+            <p className="mt-24 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
+              <a href="https://hemptemps.com/terms-and-conditions/" target="_blank" className="underline">
+                Terms and Conditions
+              </a>
+            </p>
+          </div>
+        </footer>
+      </div>
+    </>
   )
 }
