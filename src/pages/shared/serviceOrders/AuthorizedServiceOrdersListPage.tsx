a2b8cfb89bfd205cd1b8e92ef22e74aef419c2c3
Alex Artemov <146786356+alexartemov7@users.noreply.github.com>
2024-07-30 16:13:08 -0400

    displaying job start time and end time in service order tables && removing 30 day job booking restriction

--- src/pages/shared/serviceOrders/AuthorizedServiceOrdersListPage.tsx
+++ src/pages/shared/serviceOrders/AuthorizedServiceOrdersListPage.tsx
@@ -74,6 +74,30 @@ export const AuthorizedServiceOrdersListPage = () => {
       { Header: 'Job Title', accessor: 'job_id.title' },
       { Header: 'Job UID', accessor: 'job_id.uid' },
       {
+        Header: 'Job Start Date',
+        accessor: (row: IServiceOrder) => {
+          if (row.job_id && Array.isArray(row.job_id.job_dates) && row.job_id.job_dates.length > 0) {
+            const sortedDates = [...row.job_id.job_dates].sort()
+            const earliestDate = sortedDates[0]
+            return earliestDate.split('T')[0]
+          }
+          return 'N/A'
+        },
+        id: 'job_start_date',
+      },
+      {
+        Header: 'Job End Date',
+        accessor: (row: IServiceOrder) => {
+          if (row.job_id && Array.isArray(row.job_id.job_dates) && row.job_id.job_dates.length > 0) {
+            const sortedDates = [...row.job_id.job_dates].sort()
+            const latestDate = sortedDates[sortedDates.length - 1]
+            return latestDate.split('T')[0]
+          }
+          return 'N/A'
+        },
+        id: 'job_end_date',
+      },
+      {
         Header: 'Job has ended?',
         accessor: (row: IServiceOrder) =>
           row.job_id !== null && typeof row.job_id?.is_completed !== 'undefined'
