7db33fee85d60c04c128103640883289a2fec19c
jonathans199 <jonathans199@gmail.com>
2024-08-07 12:15:34 -0400

    re-added copyright to the login form

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -86,7 +86,8 @@ export const Auth = () => {
         <footer className="bg-white py-6" aria-labelledby="footer-heading">
           <div className=" pt-8 text-center md:items-center md:justify-center">
             <p className="mt-24 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
-              © {new Date().getFullYear()} Hemp Temps
+              {' '}
+              Copyright © {new Date().getFullYear()} Hemp Temps
             </p>
             <p className="mt-24 text-xs leading-5 text-gray-500 md:order-1 md:mt-0">
               <a href="https://hemptemps.com/terms-and-conditions/" target="_blank" className="underline">
