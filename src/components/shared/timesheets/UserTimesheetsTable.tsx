7c9f1de061f5f5eee8bec44e6e1e47adee88e238
Peter Lewandowski <96852364+peterlewandowski@users.noreply.github.com>
2024-08-15 10:47:36 -0400

    chore: update time displays and time editing with timezone, show time data in ascending order in UserTimesheetsTable

--- src/components/shared/timesheets/UserTimesheetsTable.tsx
+++ src/components/shared/timesheets/UserTimesheetsTable.tsx
@@ -3,6 +3,7 @@ import { useCallback, useEffect, useMemo, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
 
 import { format, isToday, isValid } from 'date-fns'
+import { formatInTimeZone, fromZonedTime, toZonedTime } from 'date-fns-tz'
 import { Calendar } from 'primereact/calendar'
 import { Column, type ColumnEditorOptions, type ColumnEvent } from 'primereact/column'
 import { DataTable, type DataTableExpandedRows, type DataTableValueArray } from 'primereact/datatable'
@@ -15,7 +16,7 @@ import { requestService } from '../../../services/requestServiceNew'
 import { useUtils } from '../../../store/useUtils'
 import { cn } from '../../../utils/cn'
 import { roleChecker } from '../../../utils/roleChecker'
-import { formatToDateTime, formatToTime } from '../../../utils/timeUtils'
+import { formatToDateTime } from '../../../utils/timeUtils'
 import { HtInputLabel } from '../forms/HtInputLabel'
 import {
   type IPunchPair,
@@ -38,7 +39,7 @@ interface IPayPeriod {
 }
 
 const cols: IAdminUserTimesheetsColumnMeta<IPunchPairsWithData>[] = [
-  { field: 'day', header: 'Day', sortable: true },
+  { field: 'day', header: 'Day', sortable: false },
   { field: 'in_time', header: 'First In', sortable: false },
   { field: 'out_time', header: 'Last Out', sortable: false },
   { field: 'lunch_time', header: 'Lunch Break', sortable: false },
@@ -156,7 +157,7 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
   }, [selectedPayPeriod, fetchTimesheets])
 
   const sortedTimeSheets = useMemo(() => {
-    return [...processedTimeSheets].sort((a, b) => new Date(b.time_stamp).getTime() - new Date(a.time_stamp).getTime())
+    return [...processedTimeSheets].sort((a, b) => new Date(a.time_stamp).getTime() - new Date(b.time_stamp).getTime())
   }, [processedTimeSheets])
 
   const cellEditor = (options: ColumnEditorOptions) => {
@@ -181,10 +182,11 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
     }
 
     const cellTimeValue = options.value ?? new Date(timeStamp)
+    const zonedCellTimeValue = toZonedTime(cellTimeValue, options.rowData.facility_timezone)
 
     return (
       <Calendar
-        value={cellTimeValue}
+        value={zonedCellTimeValue}
         hourFormat="12"
         invalid={!isEditorValid}
         onChange={e => {
@@ -266,13 +268,14 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
 
           if (field === 'in_time' || field === 'out_time') {
             const newTimeStamp: string = newValueDateString
+            const utcTimestamp = fromZonedTime(newTimeStamp, rowData.facility_timezone)
             const punchId = field === 'in_time' ? rowData.in_id : rowData.out_id
             const timeSheetId = rowData.timesheet_id
 
             const body =
               field === 'in_time'
-                ? { punch_in_id: punchId, new_time_stamp_in: newTimeStamp }
-                : { punch_out_id: punchId, new_time_stamp_out: newTimeStamp }
+                ? { punch_in_id: punchId, new_time_stamp_in: utcTimestamp }
+                : { punch_out_id: punchId, new_time_stamp_out: utcTimestamp }
 
             const response = await requestService({
               path: `timesheets/${timeSheetId}/in-out-punches`,
@@ -284,7 +287,7 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
               await handleToastAndFetchTimesheets({
                 severity: 'success',
                 summary: `Updated ${field === 'in_time' ? 'In' : 'Out'} time`,
-                detail: formatToTime(newTimeStamp),
+                detail: formatInTimeZone(utcTimestamp, rowData.facility_timezone, 'hh:mm a (z)'),
               })
             }
           }
@@ -347,7 +350,7 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
       if (Array.isArray(value)) {
         return value.map((punch: IPunchDetails) => punch[field as keyof IPunchDetails]).join(', ')
       } else if (value instanceof Date) {
-        return format(value, 'hh:mm a')
+        return formatInTimeZone(value, rowData.facility_timezone, 'hh:mm a (z)')
       } else {
         return value
       }
@@ -377,9 +380,7 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
         field: 'in_time',
         header: 'Punch In',
         body: (rowData: IPunchDetails) =>
-          rowData.in_time
-            ? rowData.in_time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })
-            : 'Not recorded',
+          rowData.in_time ? formatInTimeZone(rowData.in_time, data.facility_timezone, 'hh:mm a (z)') : 'Not recorded',
       },
       currentUserRole === 'admin' && {
         field: 'in_notes',
@@ -391,7 +392,7 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
         header: 'Punch Out',
         body: (rowData: IPunchDetails) =>
           rowData.out_time
-            ? rowData.out_time.toLocaleTimeString('en-US', { hour: '2-digit', minute: '2-digit' })
+            ? formatInTimeZone(rowData.out_time, data.facility_timezone, 'hh:mm a (z)')
             : rowData.in_time && isToday(rowData.in_time)
               ? 'Clocked In'
               : 'Clock Out not recorded',
@@ -406,7 +407,7 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
     return (
       <div className="p-4">
         <h5 className="text-sm font-semibold">Punch Details</h5>
-        <DataTable value={data.punchesWithDetails} sortField="in_time" sortOrder={-1} editMode="cell">
+        <DataTable value={data.punchesWithDetails} editMode="cell">
           {punchColumns.map(column => {
             if (typeof column === 'object' && column.body != null) {
               return (
@@ -478,13 +479,7 @@ export const UserTimesheetsTable: React.FC<IUserTimesheetsProps> = ({ selectedUs
         }}>
         <Column expander={allowExpansion} />
         {cols.map(col => (
-          <Column
-            key={col.field}
-            field={col.field}
-            header={col.header}
-            body={rowData => getCellValue(col, rowData)}
-            sortable={col.sortable}
-          />
+          <Column key={col.field} field={col.field} header={col.header} body={rowData => getCellValue(col, rowData)} />
         ))}
       </DataTable>
     </>
