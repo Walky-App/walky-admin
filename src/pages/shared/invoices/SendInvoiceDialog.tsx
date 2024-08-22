2c9229f207b8e3fd7d6ef0b8b4ef87c637827cac
Jorge Poveda <jpoveda731@gmail.com>
2024-08-22 12:01:39 -0500

    feat: add custon email send, validation and error handling in invoice page

--- src/pages/shared/invoices/SendInvoiceDialog.tsx
+++ src/pages/shared/invoices/SendInvoiceDialog.tsx
@@ -7,24 +7,92 @@ import { Tag } from 'primereact/tag'
 
 import { XMarkIcon } from '@heroicons/react/24/outline'
 
+import { type IServiceInvoice } from '../../../interfaces/serviceInvoice'
+import { requestService } from '../../../services/requestServiceNew'
+import { useUtils } from '../../../store/useUtils'
+
 interface SendInvoiceDialogProps {
-  visible: boolean
+  invoiceId: string | undefined
+  setInvoice: (invoice: IServiceInvoice | null) => void
+  setIsLoading(status: boolean): void
   setVisible: (visible: boolean) => void
+  visible: boolean
 }
 
-export const SendInvoiceDialog = ({ visible, setVisible }: SendInvoiceDialogProps) => {
+export const SendInvoiceDialog = ({
+  invoiceId,
+  setInvoice,
+  setIsLoading,
+  setVisible,
+  visible,
+}: SendInvoiceDialogProps) => {
+  const { showToast } = useUtils()
   const [emails, setEmails] = useState<string[]>([])
   const [inputEmail, setInputEmail] = useState<string>('')
+  const [emailError, setEmailError] = useState<string>('')
 
   const removeEmail = (index: number) => {
     setEmails(emails.filter((_, i) => i !== index))
-    addEmail()
   }
 
   const addEmail = () => {
-    setEmails([...emails, inputEmail])
-    setInputEmail('')
+    if (!validateEmail(inputEmail)) {
+      setEmailError('Invalid email address')
+    } else if (emails.includes(inputEmail)) {
+      setEmailError('Email already exists')
+    } else {
+      setEmails([...emails, inputEmail])
+      setInputEmail('')
+      setEmailError('')
+    }
   }
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      addEmail()
+    }
+  }
+
+  const validateEmail = (email: string) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return re.test(String(email).toLowerCase())
+  }
+
+  const clearStates = () => {
+    setEmails([])
+    setInputEmail('')
+    setEmailError('')
+    setVisible(false)
+  }
+
+  const handlerSendEmails = async () => {
+    try {
+      setIsLoading(true)
+      const response = await requestService({
+        path: `invoices/send-email/${invoiceId}`,
+        method: 'POST',
+        body: JSON.stringify({
+          emails,
+        }),
+      })
+      if (!response.ok) {
+        throw new Error('Failed to send email')
+      }
+      const fetchedData = await response.json()
+      setInvoice(fetchedData)
+      clearStates()
+      showToast({
+        severity: 'success',
+        summary: 'Completed',
+        detail: 'Email sent successfully',
+      })
+      setIsLoading(false)
+    } catch (error) {
+      console.error('Error sending email:', error)
+      setIsLoading(false)
+    }
+  }
+
   return (
     <Dialog
       visible={visible}
@@ -34,23 +102,33 @@ export const SendInvoiceDialog = ({ visible, setVisible }: SendInvoiceDialogProp
       onHide={() => setVisible(false)}
       className="md:w-4/5 lg:w-10/12">
       <div className="flex w-full flex-col items-center rounded-lg bg-white px-4 py-5 xl:px-8">
-        <div className="flex flex-wrap gap-2">
+        <div className="flex flex-wrap justify-center gap-2">
           {emails.map((email, index) => (
-            <Tag className="basis-1/4 bg-gray-400 font-black" key={index}>
-              <div className="align-items-center flex gap-1">
-                <span className="text-base">{email}</span>
-                <XMarkIcon className="h-5 w-5 cursor-pointer font-black" onClick={() => removeEmail(index)} />
+            <Tag className="basis-1/4 bg-gray-300 " key={index}>
+              <div className="align-items-center flex items-center justify-center gap-1">
+                <span className="text-base text-black">{email}</span>
+                <XMarkIcon className="h-5 w-5 cursor-pointer text-red-500" onClick={() => removeEmail(index)} />
               </div>
             </Tag>
           ))}
         </div>
 
-        <div>
-          <InputText placeholder="Email" className="w-full" value={inputEmail} />
-          <Button label="Add" className="mt-2" />
+        <div className="mt-3 flex items-center justify-center gap-2">
+          <div>
+            <InputText
+              placeholder="Email"
+              type="email"
+              className="w-full"
+              value={inputEmail}
+              onChange={e => setInputEmail(e.target.value)}
+              onKeyDown={handleKeyDown}
+            />
+            <div className="text-sm text-red-500">{emailError}</div>
+          </div>
+          <Button label="Add" onClick={addEmail} />
         </div>
         <div className="mt-4 lg:ml-auto">
-          <Button disabled={emails.length === 0} size="large" label="Confirm" onClick={() => setVisible(false)} />
+          <Button disabled={emails.length === 0} size="large" label="Confirm" onClick={handlerSendEmails} />
         </div>
       </div>
     </Dialog>
