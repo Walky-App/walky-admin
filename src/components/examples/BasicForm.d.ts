6eec135fa9dccc1b434d324ffe101c3226489241
Gal Gordon <gal.gordon@gmail.com>
2025-05-07 17:13:34 -0300

    testing husky

---
+++ src/components/examples/BasicForm.d.ts
@@ -0,0 +1,18 @@
+/**
+ * This is a placeholder component to show how to create a form using CoreUI components.
+ *
+ * In a real implementation, you would use CoreUI components like:
+ * import {
+ *   CForm,
+ *   CFormLabel,
+ *   CFormInput,
+ *   CFormSelect,
+ *   CFormCheck,
+ *   CButton,
+ *   CCard,
+ *   CCardBody
+ * } from '@coreui/react';
+ */
+declare function BasicForm(): import("react/jsx-runtime").JSX.Element;
+export default BasicForm;
+//# sourceMappingURL=BasicForm.d.ts.map
---
+++ src/main.d.ts
@@ -0,0 +1,3 @@
+import '@coreui/coreui/dist/css/coreui.min.css';
+import './index.css';
+//# sourceMappingURL=main.d.ts.map
