72981845eed51f52f94f1920112b597386c9fa90
Gal Gordon <gal.gordon@gmail.com>
2025-05-07 21:03:28 -0300

    added theme and dark mode

---
+++ src/theme.d.ts
@@ -0,0 +1,23 @@
+import { CSSProperties } from 'react';
+export interface ThemeColors {
+    bodyBg: string;
+    bodyColor: string;
+    cardBg: string;
+    borderColor: string;
+    primary: string;
+    secondary: string;
+    success: string;
+    info: string;
+    warning: string;
+    danger: string;
+    textMuted: string;
+    chartLine: string;
+    chartPoint: string;
+}
+export interface AppTheme {
+    colors: ThemeColors;
+    isDark: boolean;
+}
+export declare const getTheme: (isDarkMode: boolean) => AppTheme;
+export type StyleCreator<Props = object> = (theme: AppTheme, props?: Props) => CSSProperties;
+//# sourceMappingURL=theme.d.ts.map
