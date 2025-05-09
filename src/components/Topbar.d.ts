7ce531742c5f0e1efe32dcbd726ce1f194430605
Meagan Alfaro <meaganalfaro1@gmail.com>
2025-05-09 15:46:40 -0400

    fixed responsiveness and added hamburger button functionality

--- src/components/Topbar.d.ts
+++ src/components/Topbar.d.ts
@@ -1,2 +1,6 @@
-export declare const Topbar: () => import("react/jsx-runtime").JSX.Element;
+type TopbarProps = {
+    onToggleSidebar: () => void;
+};
+export declare const Topbar: ({ onToggleSidebar }: TopbarProps) => import("react/jsx-runtime").JSX.Element;
+export {};
 //# sourceMappingURL=Topbar.d.ts.map
