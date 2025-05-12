0dff1357cd8ae46c79f5744fb73e96fb3d316ba5
saraivah <smarcelin.2004@gmail.com>
2025-05-12 16:22:31 -0400

    upadted breadcrumbs with nav bar

---
+++ src/components/Breadcrumbs.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react'
+
+export const BreadcrumbDividersExample = () => {
+  return (
+    <CBreadcrumb style={{ '--cui-breadcrumb-divider': '">"' } as React.CSSProperties}>
+      <CBreadcrumbItem href="#">Home</CBreadcrumbItem>
+      <CBreadcrumbItem active>Library</CBreadcrumbItem>
+      <CBreadcrumbItem active>Example</CBreadcrumbItem>
+    </CBreadcrumb>
+  )
+}
