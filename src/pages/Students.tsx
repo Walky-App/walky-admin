d3cd485e2ef0064d182d58c5280d7fe9ce28f725
Meagan Alfaro <meaganalfaro1@gmail.com>
2025-05-12 16:40:18 -0400

    Created info widgets for the students page

--- src/pages/Students.tsx
+++ src/pages/Students.tsx
@@ -1,13 +1,26 @@
-
+import * as icon from '@coreui/icons'
+import { CCol, CRow } from '@coreui/react'
+import InfoStatWidget from '../components/InfoStatWidget' // Adjust path as needed
 
 const Students = () => {
-    return (
-      <div style={{ padding: '2rem' }}>
-        <h1>Students Page</h1>
-        <p>Add content in this page</p>
-      </div>
-    );
-  };
-  
-  export default Students;
-  
+  const widgets = [
+    { icon: icon.cilPeople, value: '1,283', label: 'Total Students', tooltip: 'Amount of Students' },
+    { icon: icon.cilBirthdayCake, value: '22', label: 'Average Age', tooltip: 'Mean age of students' },
+    { icon: icon.cilLanguage, value: '37', label: 'Languages', tooltip: 'The number of different spoken languages' },
+    { icon: icon.cilPushchair, value: '111', label: 'Parents', tooltip: 'Students who are parents' },
+  ]
+
+  return (
+    <div style={{ padding: '2rem' }}>
+      <CRow>
+        {widgets.map((w, idx) => (
+          <CCol xs={12} sm={6} md={3} className="mb-3" key={idx}>
+            <InfoStatWidget {...w} />
+          </CCol>
+        ))}
+      </CRow>
+    </div>
+  )
+}
+
+export default Students
