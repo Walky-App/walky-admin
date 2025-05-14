dfd3fa6fe19ad22eb26d21f76a07e020bbe8a191
saraivah <smarcelin.2004@gmail.com>
2025-05-14 11:37:09 -0400

    upated breadcrumbs /nav bar

--- src/components/examples/BreadCrumbs.tsx
+++ src/components/examples/BreadCrumbs.tsx
@@ -1,18 +1,43 @@
-import React from 'react'
-import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react'
+import { CBreadcrumb, CBreadcrumbItem } from '@coreui/react';
+import { Breadcrumb } from 'react-bootstrap';
+import { useLocation, Link } from 'react-router-dom';
 
+export function BreadcrumbDividersExample() {
+  const location = useLocation();
+  const pathnames = location.pathname.split('/').filter(Boolean);
 
-export const BreadcrumbDividersExample = () => {
-
-    
   return (
-    
-    <CBreadcrumb style={{ '--cui-breadcrumb-divider': '"/"' } as React.CSSProperties}>
-        <CBreadcrumb className="custom-breadcrumb"></CBreadcrumb>
-      <CBreadcrumbItem href="#">Home</CBreadcrumbItem>
-      <CBreadcrumbItem active>Dashboard</CBreadcrumbItem>
-      
-      
+   
+    <CBreadcrumb
+    className="mb-3"
+      style={{ '--cui-breadcrumb-divider': `'>'` } as React.CSSProperties}
+      >
+      <CBreadcrumbItem>
+        <Link to="/">Home</Link>
+      </CBreadcrumbItem>
+
+      {/* If on root path, show Dashboard */}
+      {location.pathname === '/' ? (
+        <CBreadcrumbItem active>Dashboard</CBreadcrumbItem>
+      ) : (
+        pathnames.map((value, index) => {
+          const to = `/${pathnames.slice(0, index + 1).join('/')}`;
+          const label = decodeURIComponent(value);
+
+          return (
+            <CBreadcrumbItem key={to} active={index === pathnames.length - 1}>
+              {index === pathnames.length - 1 ? (
+                label.charAt(0).toUpperCase() + label.slice(1)
+              ) : (
+                <Link to={to}>{label.charAt(0).toUpperCase() + label.slice(1)}</Link>
+              )}
+            </CBreadcrumbItem>
+          );
+        })
+      )}
     </CBreadcrumb>
-  )
+   
+  );
 }
+
+export default Breadcrumb
