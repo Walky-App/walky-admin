39129318987d0fbb80fa0d426ef633da273e5660
llaurenstone <163789596+llaurenstone@users.noreply.github.com>
2025-05-15 11:42:22 -0400

    worked on hyperlink

---
+++ src/pages/Engagement.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+const Engagement = () => {
+    return (_jsxs("div", { style: { padding: '2rem' }, children: [_jsx("h2", { children: "Engagement Page" }), _jsx("p", { children: "This is the Engagement page content." })] }));
+};
+export default Engagement;
---
+++ src/pages/Mywalky.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+const Mywalky = () => {
+    return (_jsxs("div", { style: { padding: '2rem' }, children: [_jsx("h2", { children: "My Walky Page" }), _jsx("p", { children: "This is the My walky page content." })] }));
+};
+export default Mywalky;
