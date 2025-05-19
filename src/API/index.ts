c8a34c1ccab92069f4109252671bdfd1044b3f2c
shmablo <Pramos187@hotmail.com>
2025-05-19 11:31:45 -0400

    Pushing what i have right now

--- src/API/index.ts
+++ src/API/index.ts
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://staging.walkyapp.com/",
+    baseURL: "staging.walkyapp.com",
+    headers: {
+    // This needs to be not hard coded
+    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmY3ZjNlODFjYTYwOGU5ZTc2NDdlYzEiLCJlbWFpbCI6ImdhbEB3YWxreWFwcC5jb20iLCJyb2xlIjoiNzg5MSIsImlhdCI6MTc0NjU2MTk3OCwiZXhwIjoxNzU0MzM3OTc4fQ.P_tyqdJk5bF0HaoFTcZG_IE35BrfccVkmG_UFkpBTZU'
+  }
 });
 
 export default API;
