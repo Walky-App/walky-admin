00ef5b78143068f654199e6c4146b99514405596
shmablo <Pramos187@hotmail.com>
2025-05-19 12:00:43 -0400

    Taking out the auth key lol

--- src/API/index.ts
+++ src/API/index.ts
@@ -4,7 +4,7 @@ const API = axios.create({
     baseURL: "https://staging.walkyapp.com",
     headers: {
     // This needs to be not hard coded
-    'Authorization': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmY3ZjNlODFjYTYwOGU5ZTc2NDdlYzEiLCJlbWFpbCI6ImdhbEB3YWxreWFwcC5jb20iLCJyb2xlIjoiNzg5MSIsImlhdCI6MTc0NjU2MTk3OCwiZXhwIjoxNzU0MzM3OTc4fQ.P_tyqdJk5bF0HaoFTcZG_IE35BrfccVkmG_UFkpBTZU'
+    'Authorization': 'lol'
   }
 });
 
