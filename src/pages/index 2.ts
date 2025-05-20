0a757fbfc500d49aeb66955f82ab38c254567399
Meagan Alfaro <meaganalfaro1@gmail.com>
2025-05-20 14:10:45 -0400

    Added data to Students page table

--- src/pages/index 2.ts
+++
@@ -1,3 +0,0 @@
-
-export * as Compliance from  './Compliance.tsx'
-export * as Login from  './Login.tsx'
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,8 @@
-
 export * as Compliance from  './Compliance.tsx'
+export * as CreateAccount from "./CreateAccount.tsx"
+export * as Engagement from "./Engagement.tsx"
 export * as Login from  './Login.tsx'
+export * as Mywalky from "./Mywalky.tsx"
+export * as Review from "./Mywalky.tsx"
+export * as Settings from "./Settings.tsx"
+export * as Students from "./Students.tsx"
