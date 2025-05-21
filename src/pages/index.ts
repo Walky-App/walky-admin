989764d839ae82b504d8d4c436fe61985db9e771
Claudiae09 <claudiae092004@gmail.com>
2025-05-21 10:36:31 -0400

    deleted extra pages and finished up top bar

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,8 +1,4 @@
-export * as Compliance from  './Compliance.tsx'
 export * as CreateAccount from "./CreateAccount.tsx"
 export * as Engagement from "./Engagement.tsx"
 export * as Login from  './Login.tsx'
-export * as Mywalky from "./Mywalky.tsx"
-export * as Review from "./Mywalky.tsx"
-export * as Settings from "./Settings.tsx"
 export * as Students from "./Students.tsx"
