b637280f7d8568ff2cba415e577fbeee8e164f27
Gustavo Bacellar <61608608+gustavobacellarladeira@users.noreply.github.com>
2025-07-30 14:58:49 -0300

    fix/meagan issue

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,7 +1,9 @@
-import '@testing-library/jest-dom';
-import { TextEncoder, TextDecoder } from 'util';
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+import "@testing-library/jest-dom";
+// @ts-expect-error
+import { TextEncoder, TextDecoder } from "util";
 
-if (typeof globalThis.TextEncoder === 'undefined') {
+if (typeof globalThis.TextEncoder === "undefined") {
   globalThis.TextEncoder = TextEncoder;
   globalThis.TextDecoder = TextDecoder;
 }
